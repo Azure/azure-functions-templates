f6a9d63d86e3604f4b9f6813e9db86185c091ccc
davidebbo <david.ebbo@microsoft.com>
2016-03-10 15:30:29 -0800

    Remove unneeded quotes

--- Templates/WebHook-Generic/index.js
+++ Templates/WebHook-Generic/index.js
@@ -3,12 +3,12 @@
 
     // Check if we got first/last properties
     if("first" in data && "last" in data) {
-        context.res = { "body": {"greeting": 'Hello ' + data.first + " " + data.last + "!"}};
+        context.res = { body: { greeting: 'Hello ' + data.first + " " + data.last + "!"}};
     }
     else {
         context.res = {
-            "status": 400,
-            "body": {"error": 'Please pass first/last properties in the input object'}
+            status: 400,
+            body: { error: 'Please pass first/last properties in the input object'}
         };
     }
 
