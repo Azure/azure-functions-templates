de6b57b3f87483c9c587c800fe7cdfdedc9ffa0d
christopheranderson <chrande@microsoft.com>
2016-03-10 10:55:24 -0800

    Use JSON message for res

--- Templates/WebHook-Generic/index.js
+++ Templates/WebHook-Generic/index.js
@@ -1,6 +1,6 @@
 ﻿module.exports = function (context, data) {
     context.res = {};
     context.log('Webhook was triggered!');
-    context.res.body = 'WebHook processed successfully! \n' + JSON.stringify(data, null, " ");
+    context.res = { body: 'WebHook processed successfully! \n' + JSON.stringify(data, null, " ") };
     context.done();
 }
--- Templates/WebHook-GitHub/index.js
+++ Templates/WebHook-GitHub/index.js
@@ -1,6 +1,5 @@
 ﻿module.exports = function (context, data) {
-    context.res = {};
     context.log('GitHub WebHook triggered! ' + data.comment.body);
-    context.res.body = 'New GitHub comment: ' + JSON.stringify(data.comment.body, null, " ");
+    context.res = {body: 'New GitHub comment: ' + JSON.stringify(data.comment.body, null, " ") };
     context.done();
 }
