2e0fdd4a0f6cfdae098962967c3bf5333a2096d4
davidebbo <david.ebbo@microsoft.com>
2016-03-10 15:25:42 -0800

    Improve the JS WebHook template

--- Templates/WebHook-Generic/index.js
+++ Templates/WebHook-Generic/index.js
@@ -1,6 +1,18 @@
 ﻿module.exports = function (context, data) {
-    context.res = {};
     context.log('Webhook was triggered!');
-    context.res = { body: 'WebHook processed successfully! \n' + JSON.stringify(data, null, " ") };
+
+    // Check if we got first/last properties
+    if('first' in data && 'last' in data) {
+        context.res = {
+            body: { greeting: 'Hello ' + data.first + ' ' + data.last + '!'}
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: { error: 'Please pass first/last properties in the input object'}
+        };
+    }
+
     context.done();
 }
