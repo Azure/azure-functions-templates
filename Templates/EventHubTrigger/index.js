9dc188c08a562796712e4e19d215b10cdab3e8ca
christopheranderson <chrande@microsoft.com>
2016-03-09 20:49:57 -0800

    Add name for EventHubs Trigger

--- Templates/EventHubTrigger/index.js
+++ Templates/EventHubTrigger/index.js
@@ -1,5 +1,5 @@
-﻿module.exports = function (context, workItem) {
-    context.log('Node.js eventhub trigger function processed work item ' + workItem);
+﻿module.exports = function (context, myEventHubTrigger) {
+    context.log('Node.js eventhub trigger function processed work item ' + myEventHubTrigger);
 
     context.done();
 }
