e784800b0289e6173ae9b93f31367fc8443ce2d9
Mathew Charles <mathewc@microsoft.com>
2016-03-15 14:22:28 -0700

    Updating templates for recent binding changes

--- Templates/HttpTrigger/index.js
+++ Templates/HttpTrigger/index.js
@@ -1,5 +1,5 @@
 ﻿module.exports = function (context, req) {
-    context.log('Node.js HTTP trigger function processed a request. Name=' + req.originalUrl);
+    context.log('Node.js HTTP trigger function processed a request. RequestUri=' + req.originalUrl);
 
     if (typeof req.query.name == 'undefined') {
         context.res = {
