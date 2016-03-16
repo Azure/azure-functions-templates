b7d60115313934e6178253bf6c6468300aaca3de
Naren Soni <soninaren@live.com>
2016-03-16 10:21:43 -0700

    Added parameter names to non queue items and format fixes

--- Templates/ServiceBusQueueTrigger-NodeJS/index.js
+++ Templates/ServiceBusQueueTrigger-NodeJS/index.js
@@ -1,16 +1,15 @@
-﻿module.exports = function (message, context) {
-    context.log("Node.js ServiceBus queue trigger function processed message '" + JSON.stringify(message) + "'");
+﻿module.exports = function(context, myQueueItem) {
+    context.log("Node.js ServiceBus queue trigger function processed message '" + JSON.stringify(myQueueItem) + "'");
 
-    var output = null;
-    if (message.count < 1)
-    {
+    var OutPutQueueItem = null;
+    if (myQueueItem.count < 1) {
         // write a message back to the queue that this function is triggered on
         // ensuring that we only loop on this once
-        message.count += 1;
-        output = {
-            message: JSON.stringify(message)
+        myQueueItem.count += 1;
+        OutPutQueueItem = {
+            message: JSON.stringify(myQueueItem)
         };
     }
 
-    context.done(null, output);
+    context.done(null, OutPutQueueItem);
 };
