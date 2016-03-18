1bd184556da1ee23099b93ce40a2fbd7bb616914
davidebbo <david.ebbo@microsoft.com>
2016-03-18 11:12:21 -0700

    Add test data to GitHub sample and clean it up to make it 'just work'

--- Templates/GitHubWebHook-NodeJS/index.js
+++ Templates/GitHubWebHook-NodeJS/index.js
@@ -1,5 +1,5 @@
 ﻿module.exports = function (context, data) {
     context.log('GitHub WebHook triggered! ' + data.comment.body);
-    context.res = {body: 'New GitHub comment: ' + JSON.stringify(data.comment.body, null, " ") };
+    context.res = { body: 'New GitHub comment: ' + data.comment.body };
     context.done();
-}
+};
