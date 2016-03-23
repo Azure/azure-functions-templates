0a306a20a936e8025a618bab0519ce737f168b7c
Naren Soni <soninaren@live.com>
2016-03-23 16:17:57 -0700

    replacing tabs with spaces and adding format string

--- Templates/HttpTrigger-NodeJS/index.js
+++ Templates/HttpTrigger-NodeJS/index.js
@@ -1,5 +1,5 @@
 ﻿module.exports = function(context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri:', req.originalUrl);
+    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
