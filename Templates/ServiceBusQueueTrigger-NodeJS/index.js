34a21e587cbe80bcb9a23932e097e3db38c3f19e
Naren Soni <soninaren@live.com>
2016-04-06 14:09:26 -0700

    renaming the default parameter for all bindings in binding.json and corresponding templates

--- Templates/ServiceBusQueueTrigger-NodeJS/index.js
+++ Templates/ServiceBusQueueTrigger-NodeJS/index.js
@@ -1,4 +1,4 @@
-﻿module.exports = function(context, myQueueItem) {
-    context.log('Node.js ServiceBus queue trigger function processed message', myQueueItem);
+﻿module.exports = function(context, sbQueueItem) {
+    context.log('Node.js ServiceBus queue trigger function processed message', sbQueueItem);
     context.done();
 };
