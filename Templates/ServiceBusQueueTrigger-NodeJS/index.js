7e3d8da6b3fed3d9c7568a066447b81091b9f5cd
Naren Soni <soninaren@live.com>
2016-04-07 13:04:51 -0700

    fixing service bus parameter name

--- Templates/ServiceBusQueueTrigger-NodeJS/index.js
+++ Templates/ServiceBusQueueTrigger-NodeJS/index.js
@@ -1,4 +1,4 @@
-﻿module.exports = function(context, sbQueueItem) {
-    context.log('Node.js ServiceBus queue trigger function processed message', sbQueueItem);
+﻿module.exports = function(context, mySbQueueMsg) {
+    context.log('Node.js ServiceBus queue trigger function processed message', mySbQueueMsg);
     context.done();
 };
