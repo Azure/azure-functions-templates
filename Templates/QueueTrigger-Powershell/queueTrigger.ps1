f92e3b2d2d581e5c2fede69e9b799d3a3be721d3
Naren Soni <soninaren@live.com>
2016-04-15 14:01:44 -0700

    Addressing PR comments

--- Templates/QueueTrigger-Powershell/queueTrigger.ps1
+++ Templates/QueueTrigger-Powershell/queueTrigger.ps1
@@ -2,5 +2,5 @@ $in = Get-Content $Env:input
 $output = $Env:output
 
 [Console]::WriteLine("Powershell script processed queue message '$in'")
-$entity = [string]::Format('{{ "timestamp": "{0}", "title": "queue message:{1}" }}', $(get-date).ToString(), $in)
+$entity = [string]::Format('{{ "timestamp": "{0}", "title": "queue message: {1}" }}', $(get-date).ToString(), $in)
 $entity | Out-File -Encoding Ascii $output
