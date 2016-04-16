ebfbc77c0a9edfe3466c3657da6528d3c1dbf3ca
Naren Soni <soninaren@users.noreply.github.com>
2016-04-15 17:53:50 -0700

    Cleaning up templates in experimental category (#149)
    Cleaning up templates in experimental category

--- Templates/QueueTrigger-Powershell/queueTrigger.ps1
+++ Templates/QueueTrigger-Powershell/queueTrigger.ps1
@@ -1,8 +1,6 @@
 $in = Get-Content $Env:input
+$output = $Env:output
 
 [Console]::WriteLine("Powershell script processed queue message '$in'")
-
-$output = $Env:output
-$json = $in | ConvertFrom-Json
-$entity = [string]::Format('{{ "timestamp": "{0}", "status": 0, "title": "Powershell Table Entity for message {1}" }}', $(get-date -f MM-dd-yyyy_HH_mm_ss), $json.id)
+$entity = [string]::Format('{{ "timestamp": "{0}", "title": "queue message: {1}" }}', $(get-date).ToString(), $in)
 $entity | Out-File -Encoding Ascii $output
