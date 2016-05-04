7c1a30bad1c7a33e46063d2d56029103e3b806f8
Naren Soni <soninaren@live.com>
2016-05-04 14:39:07 -0700

    Created Get and Set Entity for table storage via HttpTrigger

---
+++ Templates/HttpGET(CRUD)-NodeJS/index.js
@@ -0,0 +1,8 @@
+﻿module.exports = function (context, req, intable) {
+    context.log("Retrieved records:", intable);
+    context.res = {
+        status: 200,
+        body: intable
+    };
+    context.done();
+};
