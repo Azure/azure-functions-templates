44df3e6016b3b7faf3d18d1483d2df3109cda744
Naren Soni <soninaren@users.noreply.github.com>
2016-05-16 15:10:40 -0700

    Created Get and Set Entity for table storage via HttpTrigger (#197)

---
+++ Templates/HttpPOST(CRUD)-NodeJS/index.js
@@ -0,0 +1,17 @@
+﻿module.exports = function (context, req) {
+    var statusCode = 400;
+    var responseBody = "Invalid request object";
+
+    if (typeof req.body != 'undefined' && typeof req.body == 'object') {
+        statusCode = 201;
+        context.bindings.outTable = req.body;
+        responseBody = "Table Storage Created";
+    }
+
+    context.res = {
+        status: statusCode,
+        body: responseBody
+    };
+
+    context.done();
+};
