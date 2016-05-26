f4c0cd7755523848042bf74b3fef737fe4912b69
Mathew Charles <mathewc@microsoft.com>
2016-05-26 15:46:38 -0700

    Simplifying some of the standard templates

--- Templates/QueueTrigger-Powershell/run.ps1
+++ Templates/QueueTrigger-Powershell/run.ps1
@@ -1,6 +1,3 @@
 $in = Get-Content $Env:input
-$output = $Env:output
 
 [Console]::WriteLine("Powershell script processed queue message '$in'")
-$entity = [string]::Format('{{ "timestamp": "{0}", "title": "queue message: {1}" }}', $(get-date).ToString(), $in)
-$entity | Out-File -Encoding Ascii $output
