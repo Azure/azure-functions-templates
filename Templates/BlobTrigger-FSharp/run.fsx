2eede2f8c88aa8bc24fe0bb75297c5479b527d29
Naren Soni <soninaren@live.com>
2016-10-24 08:52:50 -0700

    Update blob templates to use binary blobs

--- Templates/BlobTrigger-FSharp/run.fsx
+++ Templates/BlobTrigger-FSharp/run.fsx
@@ -1,4 +1,4 @@
 open System
 
-let Run(myBlob: string, log: TraceWriter) =
-    log.Verbose(sprintf "F# Blob trigger function processed blob: %s" myBlob)
+let Run(myBlob: Stream, name: string, log: TraceWriter) =
+    log.Verbose(sprintf "F# Blob trigger function processed blob\n Name: %s \n Size: %d Bytes" name myBlob.Length)
