69edece615cfd3c2181a54bafe465e2ca9f054c8
Naren Soni <soninaren@live.com>
2016-11-10 13:46:04 -0800

    Using $return for httpTriggers

--- Templates/HttpTrigger-JavaScript/index.js
+++ Templates/HttpTrigger-JavaScript/index.js
@@ -1,17 +1,17 @@
-﻿module.exports = function(context, req) {
+﻿module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
+        res = {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
     else {
-        context.res = {
+        res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
     }
-    context.done();
+    context.done(null, res);
 };
