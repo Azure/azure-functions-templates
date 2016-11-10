2a1f28ec1da5b706f303407bd75086f49dc01974
Naren Soni <soninaren@live.com>
2016-11-10 15:10:00 -0800

    Removing non-functional Fsharp Functions

--- Templates/EventHubTrigger-FSharp/run.fsx
+++ Templates/EventHubTrigger-FSharp/run.fsx
@@ -1,4 +1,4 @@
 open System
 
-let Run(myEventHubMessage: string, log: TraceWriter)
+let Run(myEventHubMessage: string, log: TraceWriter) =
     log.Info(sprintf "F# Event Hub trigger function processed a message: %s" myEventHubMessage)
