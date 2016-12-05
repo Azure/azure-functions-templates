f794325ce34200b1b42f10ced9e97d69d588d2ab
Sylvan Clebsch <syclebsc@microsoft.com>
2016-12-05 17:52:35 +0000

    update HttpTrigger-FSharp to handle both query and body

--- Templates/HttpTrigger-FSharp/run.fsx
+++ Templates/HttpTrigger-FSharp/run.fsx
@@ -1,16 +1,30 @@
 #r "System.Net.Http"
+#r "Newtonsoft.Json"
 
 open System.Net
 open System.Net.Http
+open Newtonsoft.Json
+
+type Named = {
+    name: string
+}
 
 let Run(req: HttpRequestMessage, log: TraceWriter) =
-    log.Info(sprintf 
-        "F# HTTP trigger function processed a request.")
+    async {
+        log.Info(sprintf 
+            "F# HTTP trigger function processed a request.")
 
-    // Set name to query string
-    let name =
-        req.GetQueryNameValuePairs()
-        |> Seq.tryFind (fun q -> q.Key = "name")
-        |> fun x -> x.Value
-    
-    req.CreateResponse(HttpStatusCode.OK, "Hello " + name.Value);
+        // Set name to query string
+        let name =
+            req.GetQueryNameValuePairs()
+            |> Seq.tryFind (fun q -> q.Key = "name")
+
+        match name with
+        | Some x ->
+            return req.CreateResponse(HttpStatusCode.OK, "Hello " + x.Value);
+        | None ->
+            let! data = req.Content.ReadAsStringAsync() |> Async.AwaitTask
+            let named = JsonConvert.DeserializeObject<Named>(data)
+
+            return req.CreateResponse(HttpStatusCode.OK, "Hello " + named.name);
+    } |> Async.RunSynchronously
