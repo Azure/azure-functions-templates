234c2e46280b64a99ad3f8e133ab62401c6c87a9
Naren Soni <soninaren@live.com>
2016-12-28 15:40:49 -0800

    fixed the BlobTrigger-Batch template

--- Templates/BlobTrigger-Batch/run.bat
+++ Templates/BlobTrigger-Batch/run.bat
@@ -1,3 +1,3 @@
 echo OFF
-SET /p inputBlob=<%inputBlob%
-echo Windows Batch script processed blob '%inputBlob%'
+FOR /F "usebackq" %%i IN ('%inputBlob%') DO set size=%%~zi
+echo Blob trigger function Processed, blob size:%size% bytes
