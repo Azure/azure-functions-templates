daa9adc96c11cf81927729c1b638fba0eafaebbf
Naren Soni <soninaren@live.com>
2017-04-14 10:28:05 -0700

    Adding HttpTriggerWith Parameters template

---
+++ Templates/HttpTriggerWithParameters-JavaScript/index.js
@@ -0,0 +1,8 @@
+﻿module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Hello " + req.params.name
+    };
+    context.done();
+};
