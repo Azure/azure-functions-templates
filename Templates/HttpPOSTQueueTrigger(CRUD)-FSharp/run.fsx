d9d0dc119f10ca5aa9d11fff14aa559952861d67
Jamie Dixon <jamessdixon@gmail.com>
2017-05-15 15:43:20 -0400

    F sharp templates (#436)
    F# function that will push data to a queue on receiving a Http Post.

---
+++ Templates/HttpPOSTQueueTrigger(CRUD)-FSharp/run.fsx
@@ -0,0 +1,45 @@
+﻿#r "System.Net.Http"
+#r "Microsoft.WindowsAzure.Storage"
+#r "Newtonsoft.Json"
+
+open System
+open System.Net
+open System.Net.Http
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Queue
+
+type Person() =
+    member val id: Guid = Guid.NewGuid() with get    
+    member val firstName: string = null with get, set    
+    member val lastName: string = null with get, set    
+    member val address: string = null with get, set    
+
+let inline getValue (value:JToken) = 
+    match isNull value with
+    | false -> value.Value<string>()  
+    | true -> ""
+
+let hydrate data = 
+    let json = JObject.Parse(data)
+    let person = Person()
+    person.firstName <- getValue json.["firstName"]
+    person.lastName <- getValue json.["lastName"] 
+    person.address <- getValue json.["address"] 
+    person
+
+let isValid (person:Person) =
+    [person.firstName; person.lastName; person.address]
+    |> List.forall (not << String.IsNullOrEmpty)
+
+let Run(req: HttpRequestMessage, personItem:string byref) =
+    let data = req.Content.ReadAsStringAsync().Result
+    let person = hydrate data
+    match isValid person with
+    | true -> 
+        let queueMessage = person |> JsonConvert.SerializeObject
+        personItem <- queueMessage
+        req.CreateResponse(HttpStatusCode.Created)
+    | false ->
+        req.CreateResponse(HttpStatusCode.BadRequest, "Please pass all of the values in the request body")
