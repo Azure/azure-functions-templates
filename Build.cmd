6bbfd644857f415c56d6b038c691f793561477d3
Naren Soni <soninaren@users.noreply.github.com>
2017-06-01 16:58:55 -0700

    Creating a solution and build for templates (#463)

--- Build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-IF NOT "%2" == "" (
-    powershell -ExecutionPolicy Bypass -Command %~dpn0.ps1 -target %1 -templateVersion %2
-) ELSE IF NOT "%1" == "" (
-    powershell -ExecutionPolicy Bypass -Command %~dpn0.ps1 -target %1
-) ELSE (
-    powershell -ExecutionPolicy Bypass -Command %~dpn0.ps1
-)
---
+++ Functions.Templates/Templates/GenericWebHook-CSharp/run.cs
@@ -0,0 +1,47 @@
+#if (portalTemplates)
+#r "Newtonsoft.Json"
+
+using System;
+using System.Net;
+using Newtonsoft.Json;
+public static async Task<object> Run(HttpRequestMessage req, TraceWriter log)
+#endif
+#if (vsTemplates)
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Company.Function
+{
+    public static class GenericWebHookCSharp
+    {
+        [FunctionName("FunctionNameValue")]
+        public static async Task<object> Run([HttpTrigger(WebHookType = "genericJson")]HttpRequestMessage req, TraceWriter log)
+#endif
+        {
+            log.Info($"Webhook was triggered!");
+
+            private string _jsonContent = await req.Content.ReadAsStringAsync();
+private dynamic _data = JsonConvert.DeserializeObject(jsonContent);
+
+            if (data.first == null || data.last == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, new
+                {
+                    error = "Please pass first/last properties in the input object"
+                });
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                greeting = $"Hello {data.first} {data.last}!"
+            });
+        }
+#if (vsTemplates)
+    }
+}
+#endif
