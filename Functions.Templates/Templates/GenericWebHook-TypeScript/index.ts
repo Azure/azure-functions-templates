286a42fcaeb2e9e41637542ea36a296292a21801
Christopher Anderson <chrande@microsoft.com>
2017-06-09 16:46:20 -0700

    Adding TypeScript core templates (#469)

---
+++ Functions.Templates/Templates/GenericWebHook-TypeScript/index.ts
@@ -0,0 +1,23 @@
+﻿export function run(context: any, data: IData): void {
+    context.log("Webhook was triggered!");
+
+    // check if we got first/last properties
+    if ("first" in data && "last" in data) {
+        context.res = {
+            body: { greeting: `Hello ${data.first} ${data.last}!` }
+        };
+    } else {
+        context.res = {
+            status: 400,
+            body: { error: "Please pass first/last properties in the input object" }
+        };
+    }
+
+    context.done();
+}
+
+interface IData {
+    first: string;
+    last: string;
+}
+
---
+++ Functions.Templates/Templates/TimerTrigger-TypeScript/index.ts
@@ -0,0 +1,10 @@
+﻿export function run(context: any, myTimer: any): any {
+    const timeStamp: string = new Date().toISOString();
+
+    if(myTimer.isPastDue) {
+        context.log(`TypeScript is running late!`);
+    }
+    context.log(`TypeScript timer trigger function ran! ${timeStamp}`);
+
+    context.done();
+};
