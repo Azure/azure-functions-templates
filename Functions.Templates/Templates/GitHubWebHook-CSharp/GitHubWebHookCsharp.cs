3a6051d02328c8ee925ab76e28d00b2a520b2515
Naren Soni <soninaren@live.com>
2017-07-20 13:12:06 -0700

    renaming run.cs for all CSharp templates to their respective source names

--- Functions.Templates/Templates/GitHubWebHook-CSharp/GitHubWebHookCsharp.cs
+++ Functions.Templates/Templates/GitHubWebHook-CSharp/GitHubWebHookCsharp.cs
@@ -14,9 +14,9 @@ using Newtonsoft.Json;
 
 namespace Company.Function
 {
-    public static class GenericWebHookCSharp
+    public static class GitHubWebHookCsharp
     {
-        [FunctionName("GenericWebHookCSharp")]
+        [FunctionName("GitHubWebHookCsharp")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(WebHookType = "github")]HttpRequestMessage req, TraceWriter log)
 #endif
         {
