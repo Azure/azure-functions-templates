86c80d23ba5573e35889585706c93a758b3938be
Naren Soni <soninaren@live.com>
2017-09-19 18:10:53 -0700

    updating the HttpTriggerCSharp to use request body or query string

--- Functions.Templates/Templates/HttpTrigger-CSharp/HttpTriggerCSharp.cs
+++ Functions.Templates/Templates/HttpTrigger-CSharp/HttpTriggerCSharp.cs
@@ -1,7 +1,10 @@
 #if (portalTemplates)
+#r "Newtonsoft.Json"
+
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
 
 public static IActionResult Run(HttpRequest req, TraceWriter log)
 #endif
@@ -20,12 +23,15 @@ namespace Company.Function
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            if (req.Query.TryGetValue("name", out StringValues value))
-            {
-                return new OkObjectResult($"Hello, {value.First()}");
-            }
+            string name = req.Query["name"];
 
-            return new BadRequestObjectResult("Please pass a name on the query string");
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
 #if (vsTemplates)
     }
