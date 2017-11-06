d467ef76c5a5be5b5bf8977a5bc908ca1819e495
Naren Soni <soninaren@live.com>
2017-11-06 15:16:56 -0800

    removing unnecessary using from durable functions

--- Functions.Templates/Templates/DurableFunctionsActivity-CSharp/DurableFunctionsActivityCSharp.cs
+++ Functions.Templates/Templates/DurableFunctionsActivity-CSharp/DurableFunctionsActivityCSharp.cs
@@ -1,15 +1,13 @@
+#if (portalTemplates)
 /*
  * Before running this sample, please create a Durable Activity function (default name is "hello")
  */
 
-#if (portalTemplates)
 #r "Microsoft.Azure.WebJobs.Extensions.DurableTask"
  
 public static string Run(string name)
 #endif
 #if (vsTemplates)
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 
 namespace Company.Function
