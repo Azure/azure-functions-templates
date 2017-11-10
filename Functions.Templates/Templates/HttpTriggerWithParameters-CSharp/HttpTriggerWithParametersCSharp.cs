489a624d43a2132d39628f6dfe461b5749a42ed9
Naren Soni <soninaren@live.com>
2017-11-10 12:14:30 -0800

    Updating the name of HttpTriggerWithParametersCSharp template

--- Functions.Templates/Templates/HttpTriggerWithParameters-CSharp/HttpTriggerWithParametersCSharp.cs
+++ Functions.Templates/Templates/HttpTriggerWithParameters-CSharp/HttpTriggerWithParametersCSharp.cs
@@ -15,7 +15,7 @@ namespace Company.Function
     public static class HttpTriggerWithParametersCSharp
     {
         [FunctionName("HttpTriggerWithParametersCSharp")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post", Route = "HttpTriggerCSharp/name/{name}")]HttpRequestMessage req, string name, TraceWriter log)
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post", Route = "HttpTriggerWithParametersCSharp/name/{name}")]HttpRequestMessage req, string name, TraceWriter log)
 #endif
         {
             log.Info("C# HTTP trigger function processed a request.");
