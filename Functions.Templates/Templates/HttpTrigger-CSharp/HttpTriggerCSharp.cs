0a84323d86847e1fc28371ee9c0ddab5748d910c
Steffen Forkmann <sforkmann@gmail.com>
2018-01-24 20:09:52 +0100

    Don't need to look at content if name was passed as query string (#601)

--- Functions.Templates/Templates/HttpTrigger-CSharp/HttpTriggerCSharp.cs
+++ Functions.Templates/Templates/HttpTrigger-CSharp/HttpTriggerCSharp.cs
@@ -26,13 +26,15 @@ namespace Company.Function
             string name = req.GetQueryNameValuePairs()
                 .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
                 .Value;
+            
+            if (name == null)
+            {
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
 
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-
-            // Set name to query string or body data
-            name = name ?? data?.name;
-
+                name = data?.name;
+            }
+            
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
                 : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
