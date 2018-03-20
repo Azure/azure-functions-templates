179fd34bd5eed07b5f684e35df3ddc02d840dd8f
Naren Soni <soninaren@users.noreply.github.com>
2018-03-20 14:26:09 -0700

    Adding sendgrid template to VS (#648)

---
+++ Functions.Templates/Templates/SendGrid-CSharp/SendGridCSharp.cs
@@ -0,0 +1,55 @@
+// The 'From' and 'To' fields are automatically populated with the values specified by the binding settings.
+//
+// You can also optionally configure the default From/To addresses globally via host.config, e.g.:
+//
+// {
+//   "sendGrid": {
+//      "to": "user@host.com",
+//      "from": "Azure Functions <samples@functions.com>"
+//   }
+// }
+#if (portalTemplates)
+#r "SendGrid"
+
+using System;
+using SendGrid.Helpers.Mail;
+using Microsoft.Azure.WebJobs.Host;
+
+public static SendGridMessage Run(Order order, TraceWriter log)
+#endif
+#if (vsTemplates)
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using SendGrid.Helpers.Mail;
+
+namespace Company.Function
+{
+    public static class SendGridCSharp
+    {
+        [FunctionName("SendGridCSharp")]
+        [return: SendGrid(ApiKey = "ApiKeyValue",To = "{CustomerEmail}", From = "FromEmailValue" )]
+        public static SendGridMessage Run([QueueTrigger("PathValue", Connection = "ConnectionValue")]Order order, TraceWriter log)
+#endif
+        {
+            log.Info($"C# Queue trigger function processed order: {order.OrderId}");
+
+            SendGridMessage message = new SendGridMessage()
+            {
+                Subject = $"Thanks for your order (#{order.OrderId})!"
+            };
+
+            message.AddContent("text/plain", $"{order.CustomerName}, your order ({order.OrderId}) is being processed!");
+            return message;
+        }
+#if (vsTemplates)
+    }
+#endif
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+#if (vsTemplates)
+}
+#endif
