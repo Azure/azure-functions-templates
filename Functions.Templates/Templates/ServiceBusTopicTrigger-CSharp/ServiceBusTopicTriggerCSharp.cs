c80e0735a46040b1abe1399af8afd2d721831c6d
Naren Soni <soninaren@live.com>
2018-03-21 11:34:10 -0700

    Adding servicebus template

---
+++ Functions.Templates/Templates/ServiceBusTopicTrigger-CSharp/ServiceBusTopicTriggerCSharp.cs
@@ -0,0 +1,25 @@
+#if (portalTemplates)
+using System;
+using System.Threading.Tasks;
+
+public static void Run(string mySbMsg, TraceWriter log)
+#endif
+#if (vsTemplates)
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Company.Function
+{
+    public static class ServiceBusTopicTriggerCSharp
+    {
+        [FunctionName("ServiceBusTopicTriggerCSharp")]
+        public static void Run([ServiceBusTrigger("TopicNameValue", "SubscriptionNameValue", AccessRights.AccessRightsValue, Connection = "ConnectionValue")]string mySbMsg, TraceWriter log)
+#endif
+        {
+            log.Info($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+#if (vsTemplates)
+    }
+}
+#endif
