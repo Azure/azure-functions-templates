98e069b96ec82de67062c95ab950efd8c3ae8f8c
Naren Soni <soninaren@live.com>
2018-03-28 13:27:59 -0700

    Adding cosmos db template for javascript

---
+++ Functions.Templates/Templates/CosmosDBTrigger-JavaScript/index.js
@@ -0,0 +1,6 @@
+module.exports = function (context, documents) {
+    if (!!documents && documents.length > 0) {
+        context.log('Document Id: ', documents[0].id);
+    }
+    context.done();
+}
