0d0a0ac5c13d5d77128c394af51b01c40423d063
Naren Soni <soninaren@live.com>
2018-04-04 10:57:37 -0700

    Removing unnecessary using statements from service bus templates

--- Functions.Templates/Templates/ServiceBusTopicTrigger-CSharp/ServiceBusTopicTriggerCSharp.cs
+++ Functions.Templates/Templates/ServiceBusTopicTrigger-CSharp/ServiceBusTopicTriggerCSharp.cs
@@ -7,7 +7,6 @@ public static void Run(string mySbMsg, TraceWriter log)
 #if (vsTemplates)
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.ServiceBus.Messaging;
 
 namespace Company.Function
 {
