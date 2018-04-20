b984a149579d6fd6226af6f1c82682a2cca52460
Naren Soni <soninaren@live.com>
2018-04-20 12:54:06 -0700

    Adding IotHubTriggerCSharp template for VS

---
+++ Functions.Templates/Templates/IotHubTrigger-CSharpVS/IotHubTriggerCSharp.cs
@@ -0,0 +1,21 @@
+using IoTHubTrigger = Microsoft.Azure.WebJobs.ServiceBus.EventHubTriggerAttribute;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Net.Http;
+
+namespace Company.Function
+{
+    public static class IotHubTriggerCSharp
+    {
+        private static HttpClient client = new HttpClient();
+
+        [FunctionName("IotHubTriggerCSharp")]
+        public static void Run([IoTHubTrigger("PathValue", Connection = "ConnectionValue")]EventData message, TraceWriter log)
+        {
+            log.Info($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+        }
+    }
+}
