ceff3e709b52d39faa47d5dec4bb1f087d98d9c4
Elvis Pranskevichus <elvis@magic.io>
2018-06-28 12:08:18 -0400

    Add EventHub and EventGrid templates for Python

---
+++ Functions.Templates/Templates/EventGridTrigger-Python/__init__.py
@@ -0,0 +1,16 @@
+import json
+import logging
+
+import azure.functions as func
+
+
+def main(event: func.EventGridEvent):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+
+    logging.info('Python EventGrid trigger processed an event: %s', result)
