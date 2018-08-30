1feddce8703360dca2c3594adf4696706ea00e3a
Naren Soni <soninaren@live.com>
2018-08-30 15:00:44 -0700

    Revert: Fixing the Event Grid identity property, updating the webhook url

--- Functions.Templates/Templates/EventGridTrigger-CSharp/EventGridTriggerCSharp.cs
+++ Functions.Templates/Templates/EventGridTrigger-CSharp/EventGridTriggerCSharp.cs
@@ -6,7 +6,7 @@ public static void Run(EventGridEvent eventGridEvent, ILogger log)
 #endif
 #if (vsTemplates)
 // Default URL for triggering event grid function in the local environment.
-// http://localhost:7071/runtime/webhooks/EventGridExtensionConfig?functionName={functionname}
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
