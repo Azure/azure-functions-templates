b2aebf339c04c20201f0b549c35f6e09ceb36653
Dongbo Wang <dongbow@microsoft.com>
2018-09-07 15:29:58 -0700

    Address pragna's feedback

--- Functions.Templates/Templates/HttpTrigger-PowerShell/run.ps1
+++ Functions.Templates/Templates/HttpTrigger-PowerShell/run.ps1
@@ -1,7 +1,7 @@
 # Input bindings are passed in via param block.
 param($req, $TriggerMetadata)
 
-# Write to the Azure Functions log streams as you would in a normal PowerShell script.
+# Write to the Azure Functions Trace-level log stream.
 Write-Verbose "PowerShell HTTP trigger function processed a request." -Verbose
 
 # Interact with query parameters, the body of the request, etc.
