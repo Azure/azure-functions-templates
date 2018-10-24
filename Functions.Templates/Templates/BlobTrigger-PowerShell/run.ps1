48988fe990208575eecab6757dc54ed68f12b38d
Dongbo Wang <dongbow@microsoft.com>
2018-10-23 17:21:08 -0700

    Minor fixes

--- Functions.Templates/Templates/BlobTrigger-PowerShell/run.ps1
+++ Functions.Templates/Templates/BlobTrigger-PowerShell/run.ps1
@@ -1,4 +1,5 @@
-
+# Input bindings are passed in via param block.
 param([byte[]] $myBlob, $TriggerMetadata)
 
+# Write out the blob name and size to the information log.
 Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($myBlob.Length) bytes"
--- Functions.Templates/Templates/QueueTrigger-PowerShell/run.ps1
+++ Functions.Templates/Templates/QueueTrigger-PowerShell/run.ps1
@@ -1,4 +1,6 @@
+# Input bindings are passed in via param block.
 param([string] $myQueueItem, $TriggerMetadata)
 
+# Write out the queue message and insertion time to the information log.
 Write-Host "PowerShell queue trigger function processed work item: $myQueueItem"
 Write-Host "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
