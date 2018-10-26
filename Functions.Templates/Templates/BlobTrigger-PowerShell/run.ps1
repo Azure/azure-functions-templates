cb988f182ff936891a27af52812ecab37f1a7d7c
Dongbo Wang <dongbow@microsoft.com>
2018-10-25 17:50:24 -0700

    Address pragna's comment

--- Functions.Templates/Templates/BlobTrigger-PowerShell/run.ps1
+++ Functions.Templates/Templates/BlobTrigger-PowerShell/run.ps1
@@ -1,5 +1,5 @@
 # Input bindings are passed in via param block.
-param([byte[]] $Blob, $TriggerMetadata)
+param([byte[]] $InputBlob, $TriggerMetadata)
 
 # Write out the blob name and size to the information log.
-Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($Blob.Length) bytes"
+Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($InputBlob.Length) bytes"
