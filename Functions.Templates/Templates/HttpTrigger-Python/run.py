134ab8a42040c442a3375de1c2fbd6245f8657dc
Asavari Tayal <asavaritayal@Asavaris-MacBook-Pro.local>
2018-12-05 15:12:46 -0800

    Removing experimental Python templates

--- Functions.Templates/Templates/HttpTrigger-Python/run.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-
-postreqdata = json.loads(open(os.environ['req']).read())
-response = open(os.environ['res'], 'w')
-response.write("hello world from "+postreqdata['name'])
-response.close()
