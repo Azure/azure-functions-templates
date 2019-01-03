d317fe7b57b66118b33082cd12f86426e77998f5
Torben Clasen <torbenclasen@gmail.com>
2019-01-03 23:01:47 +0100

    Updated the FunctionName to QueueTriggerFSharp; Also updated the message

--- Functions.Templates/Templates/QueueTrigger-FSharp/QueueTriggerFSharp.fs
+++ Functions.Templates/Templates/QueueTrigger-FSharp/QueueTriggerFSharp.fs
@@ -6,7 +6,7 @@ open Microsoft.Azure.WebJobs.Host
 open Microsoft.Extensions.Logging
 
 module QueueTriggerFSharp =
-    [<FunctionName("QueueTriggerCSharp")>]
+    [<FunctionName("QueueTriggerFSharp")>]
     let run([<QueueTrigger("PathValue", Connection = "ConnectionValue")>]myQueueItem: string, log: ILogger) =
-        let msg = sprintf "C# Queue trigger function processed: %s" myQueueItem
+        let msg = sprintf "F# Queue trigger function processed: %s" myQueueItem
         log.LogInformation msg
