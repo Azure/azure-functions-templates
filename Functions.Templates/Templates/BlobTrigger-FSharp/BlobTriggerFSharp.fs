329dcec391981f5d6404587d42d3e708ab47b6e9
Torben Clasen <torbenclasen@gmail.com>
2019-01-03 22:55:54 +0100

    Updated BlobTrigger to use ILogger instead of TraceWriter

--- Functions.Templates/Templates/BlobTrigger-FSharp/BlobTriggerFSharp.fs
+++ Functions.Templates/Templates/BlobTrigger-FSharp/BlobTriggerFSharp.fs
@@ -3,10 +3,11 @@ namespace Company.Function
 open System.IO
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Host
+open Microsoft.Extensions.Logging
 
 module BlobTriggerFSharp =
 
     [<FunctionName("BlobTriggerFSharp")>]
-    let run ([<BlobTrigger("PathValue/{name}", Connection = "ConnectionValue")>] myBlob: Stream, name: string, log: TraceWriter) =
+    let run ([<BlobTrigger("PathValue/{name}", Connection = "ConnectionValue")>] myBlob: Stream, name: string, log: ILogger) =
         let msg = sprintf "F# Blob trigger function Processed blob\nName: %s \n Size: %d Bytes" name myBlob.Length
-        log.Info msg
+        log.LogInformation msg
