0aa66e977f6da6c76f2f01e0ca7dd5849883c72e
Phillip Carter <pcarter@fastmail.com>
2019-04-02 22:22:15 -0700

    Flesh out F# EventHub trigger

--- Functions.Templates/Templates/EventHubTrigger-FSharp/EventHubTriggerFSharp.fs
+++ Functions.Templates/Templates/EventHubTrigger-FSharp/EventHubTriggerFSharp.fs
@@ -1,11 +1,29 @@
 namespace Company.Function
 
+open System
+open System.Text
+open System.Threading.Tasks
+open Microsoft.Azure.EventHubs
 open Microsoft.Azure.WebJobs
-open Microsoft.Azure.WebJobs.Host
 open Microsoft.Extensions.Logging
 
 module EventHuBTriggerFSharp =
     [<FunctionName("EventHubTriggerFSharp")>]
-    let run([<EventHubTrigger("eventHubNameValue", Connection="ConnectionValue")>]myEventHubMessage: string, log: ILogger) =
-        let msg = sprintf "F# Event Hub trigger function processed a message: %s" myEventHubMessage
-        log.LogInformation msg
+    let run([<EventHubTrigger("eventHubNameValue", Connection="ConnectionValue")>] events: EventData[], log: ILogger) =
+        async {
+            let exns = ResizeArray<Exception>()
+
+            for eventData in events do
+                try
+                    let msgBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count)
+                    let msg = sprintf "F# Event Hub trigger function processed a message: %s" msgBody
+                    log.LogInformation msg
+                    Task.Yield() |> Async.AwaitTask
+                with
+                | e -> exns.Add(e)
+
+            if exns.Count > 1 then
+                raise(AggregateException(exns))
+            if exns.Count = 1 then
+                raise(exns.[0])
+        } |> Async.StartAsTask
