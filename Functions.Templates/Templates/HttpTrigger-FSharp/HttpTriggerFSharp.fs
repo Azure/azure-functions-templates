bee5b956b659519231e06afb6d83c31597c4a8fb
Torben Clasen <torbenclasen@gmail.com>
2019-04-04 00:06:02 +0200

    Fixing HTTPTrigger for Fsharp

--- Functions.Templates/Templates/HttpTrigger-FSharp/HttpTriggerFSharp.fs
+++ Functions.Templates/Templates/HttpTrigger-FSharp/HttpTriggerFSharp.fs
@@ -10,13 +10,40 @@ open Newtonsoft.Json
 open Microsoft.Extensions.Logging
 
 module HttpTriggerFSharp =
+    [<CLIMutable>]
+    type NameContainer = {Name:string}
+
+    [<Literal>]
+    let Name = "Name"
+
     [<FunctionName("HttpTriggerFSharp")>]
     let run ([<HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post", Route = null)>]req: HttpRequest) (log: ILogger) =
         log.LogInformation("F# HTTP trigger function processed a request.")
 
-        let name = req.Query.["name"].[0]
+    let name = 
+        match req.Query.ContainsKey Name with
+        | true -> Some req.Query.[Name].[0]
+        | false -> None
 
-        if not(isNull name) then
-            OkObjectResult(sprintf "Hello, %s" name) :> ActionResult
-        else
-            BadRequestObjectResult("Please pass a name on the query string or in the request body") :> ActionResult
+    let reqBodyName = 
+        async {
+        use stream = new StreamReader(req.Body)
+        let! body = stream.ReadToEndAsync() |> Async.AwaitTask
+        let input = 
+            match body with
+            | b when System.String.IsNullOrWhiteSpace b -> None
+            | _ -> Some (JsonConvert.DeserializeObject<NameContainer>(body))
+
+        match input with
+        | None -> 
+            return BadRequestObjectResult "Please pass a name on the query string or in the request body" :> IActionResult
+        | Some n ->
+            return OkObjectResult (sprintf "Hello, %s" n.Name) :>IActionResult
+        }
+    
+
+    match name with
+    | Some n ->
+        OkObjectResult(sprintf "Hello, %s" n) :>IActionResult
+    | None ->
+        reqBodyName |> Async.RunSynchronously
