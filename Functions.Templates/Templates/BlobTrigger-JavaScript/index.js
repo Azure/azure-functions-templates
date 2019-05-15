ccde39f3e131d50e613bc096f4e7736a2ad3f522
soninaren <soninaren@users.noreply.github.com>
2019-05-15 12:17:30 -0700

    Updating blob trigger template for javascript

--- Functions.Templates/Templates/BlobTrigger-JavaScript/index.js
+++ Functions.Templates/Templates/BlobTrigger-JavaScript/index.js
@@ -1,3 +1,3 @@
 module.exports = async function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
 };
