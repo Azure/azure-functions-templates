92d129faeab635e8a8425198017613922a236dbe
t-eckert <t_eckert@outlook.com>
2019-06-13 14:12:24 -0700

    Rename `mytimer` to `schedule` Import _from_ `datetime` relevant modules

--- Functions.Templates/Templates/TimerTrigger-Python/__init__.py
+++ Functions.Templates/Templates/TimerTrigger-Python/__init__.py
@@ -1,15 +1,13 @@
-import datetime
 import logging
+from datetime import datetime, timezone
 
 import azure.functions as func
 
 
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = (
-        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-    )
+def main(schedule: func.TimerRequest) -> None:
+    utc_timestamp = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
 
-    if mytimer.past_due:
+    if schedule.past_due:
         logging.info("The timer is past due!")
 
     logging.info("Python timer trigger function ran at %s", utc_timestamp)
