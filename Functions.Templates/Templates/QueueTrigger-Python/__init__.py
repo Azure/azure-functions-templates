c47fafe0ec5718e06749303c29b5e0179c4e4566
t-eckert <t_eckert@outlook.com>
2019-06-13 16:18:06 -0700

    Use Black formatting

--- Functions.Templates/Templates/QueueTrigger-Python/__init__.py
+++ Functions.Templates/Templates/QueueTrigger-Python/__init__.py
@@ -4,5 +4,7 @@ import azure.functions as func
 
 
 def main(msg: func.QueueMessage) -> None:
-    logging.info('Python queue trigger function processed a queue item: %s',
-                 msg.get_body().decode('utf-8'))
+    logging.info(
+        "Python queue trigger function processed a queue item: %s",
+        msg.get_body().decode("utf-8"),
+    )
