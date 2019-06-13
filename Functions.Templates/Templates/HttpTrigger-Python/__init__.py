a564c01e04402699a6ee99cd5af08879ce9c49d3
t-eckert <t_eckert@outlook.com>
2019-06-13 16:16:40 -0700

    Return response is more explicit

--- Functions.Templates/Templates/HttpTrigger-Python/__init__.py
+++ Functions.Templates/Templates/HttpTrigger-Python/__init__.py
@@ -6,18 +6,16 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    name = None
     try:
         # Get value for `name` from parameters or JSON body.
         name = req.params.get("name") or req.get_json().get("name")
     except ValueError:
         name = None
 
-    return (
-        func.HttpResponse(f"Hello {name}!")
-        if name
-        else func.HttpResponse(
+    if name:
+        return func.HttpResponse(f"Hello {name}!")
+    else:
+        return func.HttpResponse(
             "Please pass a name on the query string or in the request body",
             status_code=400,
         )
-    )
