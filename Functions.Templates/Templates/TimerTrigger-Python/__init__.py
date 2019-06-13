25f0fdb97e127f801cba88f221f9415bc9a93e96
t-eckert <t_eckert@outlook.com>
2019-06-13 14:10:04 -0700

    Use Black auto-formatting

--- Functions.Templates/Templates/TimerTrigger-Python/__init__.py
+++ Functions.Templates/Templates/TimerTrigger-Python/__init__.py
@@ -5,10 +5,11 @@ import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    utc_timestamp = (
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    )
 
     if mytimer.past_due:
-        logging.info('The timer is past due!')
+        logging.info("The timer is past due!")
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    logging.info("Python timer trigger function ran at %s", utc_timestamp)
