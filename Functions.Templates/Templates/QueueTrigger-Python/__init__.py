9f29f2787c434fc37e50c4b6e9c12d404fdcd18c
t-eckert <t_eckert@outlook.com>
2019-06-13 16:18:36 -0700

    Rename `msg` to `message`

--- Functions.Templates/Templates/QueueTrigger-Python/__init__.py
+++ Functions.Templates/Templates/QueueTrigger-Python/__init__.py
@@ -3,7 +3,7 @@ import logging
 import azure.functions as func
 
 
-def main(msg: func.QueueMessage) -> None:
+def main(message: func.QueueMessage) -> None:
     logging.info(
         "Python queue trigger function processed a queue item: %s",
         msg.get_body().decode("utf-8"),
