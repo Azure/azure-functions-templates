e5bb49ad28480ce7174e70336208cd2bc8e62747
t-eckert <t_eckert@outlook.com>
2019-06-14 11:22:33 -0700

    Use Black formatting

--- Functions.Templates/Templates/ServiceBusTopicTrigger-Python/__init__.py
+++ Functions.Templates/Templates/ServiceBusTopicTrigger-Python/__init__.py
@@ -4,5 +4,7 @@ import azure.functions as func
 
 
 def main(msg: func.ServiceBusMessage):
-    logging.info('Python ServiceBus topic trigger processed message: %s',
-                 msg.get_body().decode('utf-8'))
+    logging.info(
+        "Python ServiceBus topic trigger processed message: %s",
+        msg.get_body().decode("utf-8"),
+    )
