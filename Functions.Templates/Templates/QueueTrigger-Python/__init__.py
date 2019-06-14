727d9a77d5edc643ea27d2fc54d556f100eb3461
t-eckert <t_eckert@outlook.com>
2019-06-13 22:13:30 -0700

    Split logging across two lines

--- Functions.Templates/Templates/QueueTrigger-Python/__init__.py
+++ Functions.Templates/Templates/QueueTrigger-Python/__init__.py
@@ -4,7 +4,9 @@ import azure.functions as func
 
 
 def main(message: func.QueueMessage) -> None:
-    logging.info(
-        "Python queue trigger function processed a queue item: %s",
-        msg.get_body().decode("utf-8"),
-    )
+    # Log the received message as plaintext.
+
+    # The `message` argument
+    message = message.get_body().decode("utf-8")
+
+    logging.info("Python queue trigger function processed a queue item: " + message)
