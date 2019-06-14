a7f9dfd989327fabbcf0e3ae07404eb5fed10de0
t-eckert <t_eckert@outlook.com>
2019-06-14 11:27:44 -0700

    Log message body and content type. Add scriptFile and entryPoint

--- Functions.Templates/Templates/ServiceBusTopicTrigger-Python/__init__.py
+++ Functions.Templates/Templates/ServiceBusTopicTrigger-Python/__init__.py
@@ -3,8 +3,12 @@ import logging
 import azure.functions as func
 
 
-def main(msg: func.ServiceBusMessage):
-    logging.info(
-        "Python ServiceBus topic trigger processed message: %s",
-        msg.get_body().decode("utf-8"),
-    )
+def main(message: func.ServiceBusMessage):
+    # Log the Service Bus Message as plaintext
+
+    message_content_type = message.content_type
+    message_body = message.get_body().decode("utf-8")
+
+    logging.info("Python ServiceBus topic trigger processed message.")
+    logging.info("Message Content Type: " + message_content_type)
+    logging.info("Message Body: " + message_body)
