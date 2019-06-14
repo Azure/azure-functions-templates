1458493d5c7a114a0aae53e03e884e9651eadeda
t-eckert <t_eckert@outlook.com>
2019-06-13 22:48:04 -0700

    Use Black formatter

--- Functions.Templates/Templates/BlobTrigger-Python/__init__.py
+++ Functions.Templates/Templates/BlobTrigger-Python/__init__.py
@@ -4,6 +4,8 @@ import azure.functions as func
 
 
 def main(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+    logging.info(
+        f"Python blob trigger function processed blob \n"
+        f"Name: {myblob.name}\n"
+        f"Blob Size: {myblob.length} bytes"
+    )
