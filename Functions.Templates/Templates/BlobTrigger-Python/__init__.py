42aea6cde2a0f732311bf76df3f68b076fa134e8
t-eckert <t_eckert@outlook.com>
2019-06-14 00:07:48 -0700

    Add logging to template

--- Functions.Templates/Templates/BlobTrigger-Python/__init__.py
+++ Functions.Templates/Templates/BlobTrigger-Python/__init__.py
@@ -4,8 +4,19 @@ import azure.functions as func
 
 
 def main(blob: func.InputStream):
-    logging.info(
-        f"Python blob trigger function processed blob \n"
-        f"Name: {blob.name}\n"
-        f"Blob Size: {blob.length} bytes"
-    )
+    # Log the name, size, URI, and contents of the blob.
+
+    logging.info("Python Blob trigger function processed blob")
+
+    blob_name = blob.name
+    blob_size = blob.length
+    blob_uri = blob.uri
+
+    logging.info("Blob name: " + blob_name)
+    logging.info("Blob size: " + blob_size + " bytes")
+    logging.info("Blob URI: " + blob_uri)
+
+    if blob.readable():
+        blob_contents = blob.read().decode("utf-8")
+
+    logging.info(blob_contents)
