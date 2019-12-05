3fa7e89082980c048e06ddcd5e9f7f7dbed38125
soninaren <nasoni@microsoft.com>
2019-12-05 10:41:16 -0800

    testSigning Job

---
+++ SignTools/RunSigningJob.ps1
@@ -0,0 +1,19 @@
+$isPr = Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER
+$directoryPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+if (-not $isPr) {
+  Compress-Archive ..\Functions.Templates\bin\VS\Release\* $directoryPath\..\buildoutput\tosign.zip
+
+  if ($env:SkipAssemblySigning -eq "true") {
+    "Assembly signing disabled. Skipping signing process."
+    exit 0;
+  }
+
+  $ctx = New-AzureStorageContext $env:FILES_ACCOUNT_NAME $env:FILES_ACCOUNT_KEY
+  Set-AzureStorageBlobContent "$directoryPath/../buildoutput/tosign.zip" "azure-webjobs-extensions" -Blob "$env:APPVEYOR_BUILD_VERSION.zip" -Context $ctx
+
+  $queue = Get-AzureStorageQueue "signing-jobs" -Context $ctx
+
+  $messageBody = "SignNupkgs;azure-webjobs-extensions;$env:APPVEYOR_BUILD_VERSION.zip"
+  $queue.CloudQueue.AddMessage($messageBody)
+}
