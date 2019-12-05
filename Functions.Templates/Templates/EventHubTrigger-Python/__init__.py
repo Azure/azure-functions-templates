870ad72a7d26c337378fcfd0b13520ddfca5433a
Jeff Hollan <jeffhollan@outlook.com>
2019-12-04 22:04:01 -0800

    fix py eventhub template

--- Functions.Templates/Templates/EventHubTrigger-Python/__init__.py
+++ Functions.Templates/Templates/EventHubTrigger-Python/__init__.py
@@ -3,6 +3,7 @@ import logging
 import azure.functions as func
 
 
-def main(event: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
-                 event.get_body().decode('utf-8'))
+def main(events: func.EventHubEvent):
+    for event in events:
+        logging.info('Python EventHub trigger processed an event: %s',
+                        event.get_body().decode('utf-8'))
