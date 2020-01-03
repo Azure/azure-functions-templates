45606c7c6185255be760efa4297af9c9471ab8b4
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-01-03 15:38:56 -0800

    Use List in annotation

--- Functions.Templates/Templates/EventHubTrigger-Python/__init__.py
+++ Functions.Templates/Templates/EventHubTrigger-Python/__init__.py
@@ -1,9 +1,10 @@
+from typing import List
 import logging
 
 import azure.functions as func
 
 
-def main(events: func.EventHubEvent):
+def main(events: List[func.EventHubEvent]):
     for event in events:
         logging.info('Python EventHub trigger processed an event: %s',
                         event.get_body().decode('utf-8'))
