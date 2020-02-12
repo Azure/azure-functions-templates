27304cb84181de7d23b38a81c0e4d87f2d0f54c4
soninaren <nasoni@microsoft.com>
2020-02-12 10:59:28 -0800

    Adding script to build portal templates

---
+++ Tools/CheckResourceStrings/CheckResourceStrings/FunctionsConstants.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CheckResourceStrings
+{
+    public static class FunctionsConstants
+    {
+        public const string FunctionsCacheSectionName = "functions";
+        public const string FunctionCacheSectionName = "function";
+        public const string SystemCacheSectionName = "system";
+        public const string HostCacheSectionName = "host";
+        public const string SecretsCacheSectionName = "secrets";
+        public const string MasterKeyCacheName = "master";
+        public const string BindingsSectionName = "bindings";
+        public const string BindingTypeConfigurationName = "type";
+        public const string HttpTriggerBindingTypeName = "httpTrigger";
+        public const string HttpTriggerRouteName = "route";
+        public const string KeysSectionName = "keys";
+
+        public const string BlobSecretStorageType = "blob";
+        public const string FilesSecretStorageType = "files";
+        public const string KeyVaultSecretStorageType = "keyvault";
+
+        public const string ARMCacheFeatureFlag = "FunctionsV2ARMCacheEnabled";
+
+        public const string FunctionDisabledConfigPropertyName = "disabled";
+        public const string FunctionDisabledValue = "disabled";
+        public const string FunctionEnabledValue = "enabled";
+
+        public const string DefaultExtensionBundleSourceUri = "https://functionscdn.azureedge.net/public";
+        public const string BindingMetadataFilePath = "bindings/bindings.json";
+        public const string TemplatesCodeFilePath = "templates/templates.json";
+        public const string ResourcesFilePathFormat = "resources/Resources.{0}.json";
+        public const string ExtensionBundleDirectory = "ExtensionBundles";
+        public const string EnglishUSLocale = "en-US";
+
+        public enum TemplateDataSource
+        {
+            None,
+            HostDefault,
+            ExtensionBundle
+        }
+
+        public static Dictionary<string, string> RuntimeLanguageMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            {"dotnet", "c#"},
+            {"node", "javascript"},
+            {"powershell", "powershell"},
+            {"python", "python"}
+        };
+
+        public static ICollection<string> Locales = new Collection<string>()
+        {
+            "cs-CZ",
+            "de-DE",
+            "es-ES",
+            "fr-FR",
+            "hu-HU",
+            "it-IT",
+            "ja-JP",
+            "ko-KR",
+            "nl-NL",
+            "pl-PL",
+            "pt-BR",
+            "pt-PT",
+            "ru-RU",
+            "sv-SE",
+            "tr-TR",
+            "zh-CN",
+            "zh-TW"
+        };
+
+        public enum TemplateDataFilter
+        {
+            FilterByName,
+            None
+        }
+
+        public enum TemplateDataFormat
+        {
+            Compact,
+            Detailed
+        }
+
+        public enum TemplateContentType
+        {
+            TemplateCode,
+            BindingMetadata,
+            LocalizedStrings
+        }
+    }
+}
---
+++ Tools/CheckResourceStrings/CheckResourceStrings/Program.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace CheckResourceStrings
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Path to root of the template artifacts missing");
+            }
+
+            string templateJsonLocation = Path.Combine(args[0], "templates", "templates.json");
+            Helper.CheckJsonFilePath(templateJsonLocation);
+
+            string bindingJsonLocation = Path.Combine(args[0], "bindings", "bindings.json");
+            Helper.CheckJsonFilePath(bindingJsonLocation);
+
+            var resoucesStringNames = Helper.GetResourceStringNames(templateJsonLocation);
+            resoucesStringNames.AddRange(Helper.GetResourceStringNames(bindingJsonLocation));
+
+            string resourcesLocation = Path.Combine(args[0], "resources");
+
+            string defaultResourceFilePath = Path.Combine(resourcesLocation, $"Resources.json");
+            Helper.CheckJsonFilePath(defaultResourceFilePath);
+            var content = File.ReadAllText(defaultResourceFilePath);
+            var resourceStrings = JsonConvert.DeserializeObject<ResourceStringsObj>(content);
+            PrintMissingResourceStrings(resourceStrings.EnglishResourceMap, resoucesStringNames, defaultResourceFilePath);
+
+            defaultResourceFilePath = Path.Combine(resourcesLocation, $"Resources.en-US.json");
+            Helper.CheckJsonFilePath(defaultResourceFilePath);
+            content = File.ReadAllText(defaultResourceFilePath);
+            resourceStrings = JsonConvert.DeserializeObject<ResourceStringsObj>(content);
+            PrintMissingResourceStrings(resourceStrings.EnglishResourceMap, resoucesStringNames, defaultResourceFilePath);
+
+            foreach (var locale in FunctionsConstants.Locales)
+            {
+                string resourceFilePath = Path.Combine(resourcesLocation, $"Resources.{locale}.json");
+                Helper.CheckJsonFilePath(resourceFilePath);
+                var fileContent = File.ReadAllText(resourceFilePath);
+                var bundleresourceStrings = JsonConvert.DeserializeObject<ResourceStringsObj>(fileContent);
+                PrintMissingResourceStrings(bundleresourceStrings.LanguageResourceMap, resoucesStringNames, resourceFilePath);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void PrintMissingResourceStrings(IDictionary<string, string> resourceMap, List<string> resourceStringNames, string ResourceFileName)
+        {
+            HashSet<string> missingResources = new HashSet<string>();
+            foreach (var resourceStringName in resourceStringNames)
+            {
+                if (!resourceMap.Keys.Contains(resourceStringName))
+                {
+                    missingResources.Add(resourceStringName);
+                }
+            }
+
+            if (missingResources.Count > 0)
+            {
+                Console.WriteLine($"Following items are missing in {ResourceFileName}\n Missing Count:{missingResources.Count}");
+                foreach (var item in missingResources)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("\n\n");
+                return;
+            }
+
+            Console.WriteLine($"Nothing missing {ResourceFileName} \n\n");
+        }
+    }
+}
