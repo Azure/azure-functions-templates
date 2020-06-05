ae5250666a1f9efa6e6100b00b833c60c07eabff
drwhitey1 <58263891+drwhitey1@users.noreply.github.com>
2020-06-05 20:54:40 +0100

    Fixing typo and formatting (#950)
    Co-authored-by: Chris Whitehead <chwhite@microsoft.com>

--- Functions.Templates/Templates/TimerTrigger-PowerShell/run.ps1
+++ Functions.Templates/Templates/TimerTrigger-PowerShell/run.ps1
@@ -1,10 +1,10 @@
 # Input bindings are passed in via param block.
 param($Timer)
 
-# Get the current universal time in the default string format
+# Get the current universal time in the default string format.
 $currentUTCtime = (Get-Date).ToUniversalTime()
 
-# The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
+# The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
 if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
 }
