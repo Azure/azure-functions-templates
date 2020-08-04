1e852a2250196ff9d07343475f53ef3fb2797f4e
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-08-04 10:41:12 -0700

    Adding SignalR template as an item template for VS (#967)
    Added signalR to itemTemplates for VS

--- Functions.Templates/Templates/SignalRConnectionInfoHttpTrigger-CSharp/SignalRConnectionInfoHttpTriggerCSharp.cs
+++ Functions.Templates/Templates/SignalRConnectionInfoHttpTrigger-CSharp/SignalRConnectionInfoHttpTriggerCSharp.cs
@@ -1,31 +1,18 @@
-﻿#if (portalTemplates)
-#r "Microsoft.Azure.WebJobs.Extensions.SignalRService"
-#endif
-#if (vsTemplates)
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-#endif
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 
-#if (portalTemplates)
-public static SignalRConnectionInfo Run(HttpRequest req, SignalRConnectionInfo connectionInfo)
-{
-    return connectionInfo;
-}
-#endif
-#if (vsTemplates)
 namespace Company.Function
 {
     public static class SignalRConnectionInfoHttpTriggerCSharp
     {
         [FunctionName("negotiate")]
         public static SignalRConnectionInfo Negotiate(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
-            [SignalRConnectionInfo(HubName = "chat")] SignalRConnectionInfo connectionInfo)
+            [HttpTrigger(AuthorizationLevel.AuthLevelValue, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "HubValue")] SignalRConnectionInfo connectionInfo)
         {
             return connectionInfo;
         }
     }
 }
-#endif
