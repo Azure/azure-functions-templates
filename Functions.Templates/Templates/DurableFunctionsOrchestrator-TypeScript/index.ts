caf7b44d3fd1acd746fba54ba157f5f98843bf80
antempus <rosteele@antempus.dev>
2020-09-16 08:43:15 -0700

    add Context Type to TypeScript Orch Template

--- Functions.Templates/Templates/DurableFunctionsOrchestrator-TypeScript/index.ts
+++ Functions.Templates/Templates/DurableFunctionsOrchestrator-TypeScript/index.ts
@@ -1,17 +1,18 @@
 ﻿/*
  * This function is not intended to be invoked directly. Instead it will be
  * triggered by an HTTP starter function.
- * 
+ *
  * Before running this sample, please:
  * - create a Durable activity function (default name is "Hello")
  * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your 
+ * - run 'npm install durable-functions' from the wwwroot folder of your
  *    function app in Kudu
  */
 
 import * as df from "durable-functions"
+import { IOrchestrationFunctionContext } from 'durable-functions/lib/src/classes'
 
-const orchestrator = df.orchestrator(function* (context) {
+const orchestrator = df.orchestrator(function* (context: IOrchestrationFunctionContext) {
     const outputs = [];
 
     // Replace "Hello" with the name of your Durable Activity Function.
