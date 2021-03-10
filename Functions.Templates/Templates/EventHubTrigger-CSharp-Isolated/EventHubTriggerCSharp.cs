cb5dd6cf307185f02b313d3f1aa3b1833f2cc67d
Marie Hoeger <mahoeger@microsoft.com>
2021-03-09 21:14:45 -0800

    Update event hub trigger which defaults to cardinality many

--- Functions.Templates/Templates/EventHubTrigger-CSharp-Isolated/EventHubTriggerCSharp.cs
+++ Functions.Templates/Templates/EventHubTrigger-CSharp-Isolated/EventHubTriggerCSharp.cs
@@ -7,10 +7,10 @@ namespace Company.Function
     public static class EventHubTriggerCSharp
     {
         [Function("EventHubTriggerCSharp")]
-        public static void Run([EventHubTrigger("eventHubNameValue", Connection = "ConnectionValue")] string input, FunctionContext context)
+        public static void Run([EventHubTrigger("eventHubNameValue", Connection = "ConnectionValue")] string[] input, FunctionContext context)
         {
-            var logger = context.GetLogger("EventHubsFunction");
-            logger.LogInformation($"C# Event Hub trigger function processed a message: {input}");
+            var logger = context.GetLogger("EventHubTriggerCSharp");
+            logger.LogInformation($"First Event Hubs triggered message: {input[0]}");
         }
     }
 }
