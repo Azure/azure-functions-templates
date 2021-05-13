6db4b42d06d650c6d9861660d8bc3829d05ec96a
soninaren <nasoni@microsoft.com>
2021-05-12 17:50:53 -0700

    making unused using statement conditiona

--- Functions.Templates/Templates/HttpTriggerWithOpenAPI-CSharp/HttpTriggerWithOpenAPICSharp.cs
+++ Functions.Templates/Templates/HttpTriggerWithOpenAPI-CSharp/HttpTriggerWithOpenAPICSharp.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+#if (AuthEnabled)
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+#endif
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
