06331490bc376ce613ee244768981869b17a3a74
ijoosong <ijoosong@gmail.com>
2021-05-27 13:39:18 -0500

    Update __init__.py
    Adding comments/instructions/descriptions

--- Functions.Templates/Templates/HttpTrigger-Python/__init__.py
+++ Functions.Templates/Templates/HttpTrigger-Python/__init__.py
@@ -1,12 +1,20 @@
 import logging
 
 import azure.functions as func
-
+# Add any dependencies you might want here and in requirements.txt
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    This is the function that will be called when calling your Azure Function.
+    Currently, it is a template for an HTTP Trigger that says hello when calling your URL with a parameter 'name'
+    Try changing the code and run it locally in VS Code by pressing F5!
+    """
     logging.info('Python HTTP trigger function processed a request.')
-
+    
+    # HTTP GET request using url parameter name 'url?name=Azure'
     name = req.params.get('name')
+    
+    # HTTP POST request using json body with key 'name' and it's value
     if not name:
         try:
             req_body = req.get_json()
@@ -15,6 +23,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+    # If the program finds 'name' through the URL or as JSON body, display it!
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
