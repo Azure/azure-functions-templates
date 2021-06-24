44e0f3b1fa8755922044389f02a5e86f2645ddfe
soninaren <nasoni@microsoft.com>
2021-06-24 15:41:50 -0700

    Adding EventGrid template for track 2 sdk

---
+++ Functions.Templates/Templates/EventGridTrigger-CSharp-3.x/EventGridTriggerCSharp.cs
@@ -0,0 +1,19 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Azure.Messaging.EventGrid;
+
+namespace Company.Function
+{
+    public static class EventGridTriggerCSharp
+    {
+        [FunctionName("EventGridTriggerCSharp")]
+        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+        }
+    }
+}
