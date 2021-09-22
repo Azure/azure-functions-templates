6ae1cacb86c6b9b77e4da61752d0c365470e4de3
Francisco-Gamino <frangom@microsoft.com>
2021-09-22 16:09:29 -0700

    Replace New-OrchestrationCheckStatusResponse with New-DurableOrchestrationCheckStatusResponse

--- Functions.Templates/Templates/DurableFunctionsHttpStart-PowerShell-1.x/run.ps1
+++ Functions.Templates/Templates/DurableFunctionsHttpStart-PowerShell-1.x/run.ps1
@@ -6,5 +6,5 @@ $FunctionName = $Request.Params.FunctionName
 $InstanceId = Start-NewOrchestration -FunctionName $FunctionName
 Write-Host "Started orchestration with ID = '$InstanceId'"
 
-$Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
+$Response = New-DurableOrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
 Push-OutputBinding -Name Response -Value $Response
