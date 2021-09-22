5c0bffdf026a96d1970b79fb3a52ef37865fbfe9
Francisco-Gamino <frangom@microsoft.com>
2021-09-21 19:33:24 -0700

    Replace Start-NewOrchestration with Start-DurableOrchestration

--- Functions.Templates/Templates/DurableFunctionsHttpStart-PowerShell-2.x/run.ps1
+++ Functions.Templates/Templates/DurableFunctionsHttpStart-PowerShell-2.x/run.ps1
@@ -3,7 +3,7 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 $FunctionName = $Request.Params.FunctionName
-$InstanceId = Start-NewOrchestration -FunctionName $FunctionName
+$InstanceId = Start-DurableOrchestration -FunctionName $FunctionName
 Write-Host "Started orchestration with ID = '$InstanceId'"
 
 $Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
