98dfef56c046cb62eec81903f52d89c335cf672a
Francisco-Gamino <frangom@microsoft.com>
2021-09-22 13:49:08 -0700

    Revert PowerShell Durable Functions cmdlet renames

--- Functions.Templates/Templates/DurableFunctionsHttpStart-PowerShell-2.x/run.ps1
+++ Functions.Templates/Templates/DurableFunctionsHttpStart-PowerShell-2.x/run.ps1
@@ -3,8 +3,8 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 $FunctionName = $Request.Params.FunctionName
-$InstanceId = Start-DurableOrchestration -FunctionName $FunctionName
+$InstanceId = Start-NewOrchestration -FunctionName $FunctionName
 Write-Host "Started orchestration with ID = '$InstanceId'"
 
-$Response = New-DurableOrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
+$Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
 Push-OutputBinding -Name Response -Value $Response
