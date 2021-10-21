cc13eca7b3ccdec427c8eec6d9d5ca4619ed1aab
Matthew Henderson <mattchenderson@users.noreply.github.com>
2021-10-21 20:42:18 +0000

    Updating Blob for track 2

---
+++ Functions.Templates/Templates/BlobTrigger-CSharp-3.x/BlobTriggerCSharp.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class BlobTriggerCSharp
+    {
+        [FunctionName("BlobTriggerCSharp")]
+        public static void Run([BlobTrigger("PathValue/{name}", Connection = "ConnectionValue")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
