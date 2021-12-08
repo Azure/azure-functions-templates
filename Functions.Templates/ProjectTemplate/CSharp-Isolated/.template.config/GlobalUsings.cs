202c5e38ba0d2898a81b1b6a126429765ea2d8a6
Jon Galloway <jongalloway@gmail.com>
2021-12-08 13:20:30 -0800

    Updated for global usings

---
+++ Functions.Templates/ProjectTemplate/CSharp-Isolated/.template.config/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using global::Microsoft.Azure.Functions.Worker;
+global using global::Microsoft.Azure.Functions.Worker.Http;
+global using global::Microsoft.Extensions.Hosting;
+global using global::Microsoft.Extensions.Logging;
+global using global::System.Net;
