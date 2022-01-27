a9482a71dc3c91b69bfbd4eb607e83c60f5ae7db
soninaren <nasoni@microsoft.com>
2022-01-26 18:54:55 -0800

    Fixing a return type in SqlOutputBindingCSharp

--- Functions.Templates/Templates/SqlOutputBinding-CSharp/SqlOutputBindingCSharp.cs
+++ Functions.Templates/Templates/SqlOutputBinding-CSharp/SqlOutputBindingCSharp.cs
@@ -12,7 +12,7 @@ namespace Company.Function
     {
         // Visit https://aka.ms/sqlbindingsoutput to learn how to use this output binding
         [FunctionName("SqlOutputBinding")]
-         public static Task<IActionResult> Run(
+         public static CreatedResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "addtodoitem")] HttpRequest req,
             [Sql("table", ConnectionStringSetting = "SqlConnectionString")] out ToDoItem output,
             ILogger log)
