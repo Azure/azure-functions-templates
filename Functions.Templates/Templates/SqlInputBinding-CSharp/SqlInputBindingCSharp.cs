b22217891d4d27f8eada880b3cfe0f0e48448c0e
luczhan <luczhan@microsoft.com>
2022-02-01 15:46:18 -0800

    add sql bindings

---
+++ Functions.Templates/Templates/SqlInputBinding-CSharp/SqlInputBindingCSharp.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class SqlInputBindingCSharp
+    {
+        // Visit https://aka.ms/sqlbindingsinput to learn how to use this input binding
+        [FunctionName("SqlInputBinding")]
+         public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [Sql("SELECT * FROM object",
+            CommandType = System.Data.CommandType.Text,
+            ConnectionStringSetting = "SqlConnectionString")] IEnumerable<Object> result,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger with SQL Input Binding function processed a request.");
+
+            return new OkObjectResult(result);
+        }
+    }
+}
