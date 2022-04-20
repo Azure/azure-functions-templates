0de1ea1791d4ed40c4feb67b858a1cb55bfd8147
ShaneMicro <89389217+ShaneMicro@users.noreply.github.com>
2022-04-20 11:10:33 +0100

    Added CSharp/Python templates for C#

---
+++ Functions.Templates/Templates/AuthenticationEventsTrigge-Python/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+import azure.functions as func
+
+
+def main(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes")
