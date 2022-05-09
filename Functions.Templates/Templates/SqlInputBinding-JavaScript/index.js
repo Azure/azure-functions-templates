f549ca04ecc657a249ad803ab244335833cde534
Madhuri Koripalli <makoripa@microsoft.com>
2022-05-09 16:28:21 -0700

    add mimetype

--- Functions.Templates/Templates/SqlInputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlInputBinding-JavaScript/index.js
@@ -1,5 +1,12 @@
 module.exports = async function (context, results) {
     if (!!results && results.length > 0) {
         context.log('Query results: ', JSON.stringify(results));
+    } else {
+        context.log("No results!");
     }
+    return {
+        status: 201,
+        mimetype:"application/json",
+        body: results
+    };
 }
--- Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
@@ -1,16 +1,13 @@
-module.exports = async function (context) {
+module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const testRow = {
-        column1: "column1Value",
-        column2: "column2Value",
-        column3: "column3Value"
-    };
-
-    context.bindings.results = JSON.stringify(testRow);
-
+    var rows = req["body"]?.rows ?? [];
+    if (rows.length > 0) {
+        context.bindings.results = JSON.stringify(rows);
+    }
     return {
         status: 201,
+        mimetype: "application/json",
         body: context.bindings.results
     };
 }
