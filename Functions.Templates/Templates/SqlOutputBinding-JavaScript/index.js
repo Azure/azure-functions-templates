459547de3c40033d17b116dbc7c6c3daabe690e1
Madhuri Koripalli <makoripa@microsoft.com>
2022-05-09 14:02:57 -0700

    update testRow

--- Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
@@ -2,9 +2,9 @@ module.exports = async function (context) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const testRow = {
-        "column1": column1Value,
-        "column2": "column2Value",
-        "column3": "column3Value"
+        column1: "column1Value",
+        column2: "column2Value",
+        column3: "column3Value"
     };
 
     context.bindings.results = JSON.stringify(testRow);
