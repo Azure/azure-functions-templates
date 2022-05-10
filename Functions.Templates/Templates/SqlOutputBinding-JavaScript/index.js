083c22b89b1b42d77d84bb721ff95bea7b2ec0be
Madhuri Koripalli <makoripa@microsoft.com>
2022-05-10 13:27:07 -0700

    update http trigger data for output binding template

--- Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
@@ -2,9 +2,8 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var rows = req["body"]?.rows ?? [];
-    if (rows.length > 0) {
-        context.bindings.results = JSON.stringify(rows);
-    }
+    context.bindings.results = JSON.stringify(rows);
+	
     return {
         status: 201,
         mimetype: "application/json",
