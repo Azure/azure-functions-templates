5f0ca09d11892e4a25dbf2fb9846a07f8b936169
Madhuri Koripalli <makoripa@microsoft.com>
2022-05-11 09:22:48 -0700

    update nuspecs

--- Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
@@ -1,11 +1,12 @@
+// Visit https://aka.ms/sqlbindingsoutput to learn how to use this output binding
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('HTTP trigger with SQL output binding function processed a request.');
 
-    var rows = req["body"]?.rows ?? [];
-    context.bindings.results = JSON.stringify(rows);
-	
-    return {
-        // status: 200, /* Defaults to 200 */
-        body: context.bindings.results
+    // Set array to context.bindings for insertion
+    context.bindings.results = JSON.stringify(req.body);
+
+    context.res = {
+        status: 201,
+        body: req.body
     };
 }
