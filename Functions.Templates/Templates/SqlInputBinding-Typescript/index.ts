18e9b131cb5879c41ba46647b355c6cad270828b
chgagnon <chgagnon@microsoft.com>
2022-05-12 16:19:18 -0700

    Cleanup

--- Functions.Templates/Templates/SqlInputBinding-Typescript/index.ts
+++ Functions.Templates/Templates/SqlInputBinding-Typescript/index.ts
@@ -3,6 +3,11 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 /**
  * Sample SQL Input Binding
  * See https://aka.ms/sqlbindingsinput for more information about using this binding
+ *
+ * These values in function.json should be updated prior to running :
+ *      commandText - This should be the query to execute to retrieve the data values to return
+ *      connectionStringSetting - This should match the name of the app setting containing the connection string
+ *          to use for the SQL connection (default is SqlConnectionString)
  * @param context The Azure Function runtime context
  * @param req The HttpRequest that triggered this function
  * @param items The array of objects returned by the SQL input binding
--- Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
+++ Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
@@ -3,6 +3,11 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 /**
  * Sample SQL Output Binding
  * See https://aka.ms/sqlbindingsoutput for more information about using this binding
+ *
+ * These values in function.json should be updated prior to running :
+ *      commandText - This should be the name of the table that you wish to upsert values to
+ *      connectionStringSetting - This should match the name of the app setting containing the connection string
+ *          to use for the SQL connection (default is SqlConnectionString)
  * @param context The Azure Function runtime context
  * @param req The HttpRequest that triggered this function
  */
