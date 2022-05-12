a8896b3fc5fb2bab0e6a62c9307d9ed6473b26a2
chgagnon <chgagnon@microsoft.com>
2022-05-12 16:40:40 -0700

    improve docs

--- Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
+++ Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
@@ -4,10 +4,11 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
  * Sample SQL Output Binding
  * See https://aka.ms/sqlbindingsoutput for more information about using this binding
  *
- * These values in function.json should be updated prior to running :
- *      commandText - This should be the name of the table that you wish to upsert values to
- *      connectionStringSetting - This should match the name of the app setting containing the connection string
- *          to use for the SQL connection (default is SqlConnectionString)
+ * These tasks should be completed prior to running :
+ *      1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
+ *      2. Add an app setting named "SqlConnectionString" containing the connection string
+ *          to use for the SQL connection
+ *      3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
  * @param context The Azure Function runtime context
  * @param req The HttpRequest that triggered this function
  */
@@ -15,6 +16,8 @@ const sqlOutputBinding: AzureFunction = async function (context: Context, req: H
     context.log('HTTP trigger with SQL output binding function processed a request.');
 
     // Set items array to context.bindings for upsertion
+    // Note that this expects the body to be a JSON object or array of objects which have a property
+    // matching each of the columns in the table to upsert to.
     context.bindings.items = JSON.stringify(req.body);
 
     context.res = {
