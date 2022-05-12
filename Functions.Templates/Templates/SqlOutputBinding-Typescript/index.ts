ec3e408d4620af083bad40c86bc67b95dfbe52c6
Charles Gagnon <chgagnon@microsoft.com>
2022-05-12 10:17:12 -0700

    update comments

--- Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
+++ Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
@@ -9,11 +9,11 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const sqlOutputBinding: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger with SQL output binding function processed a request.');
 
-    // Set items array to context.bindings for insertion
+    // Set items array to context.bindings for upsertion
     context.bindings.items = JSON.stringify(req.body);
 
     context.res = {
-        status: 201,
+        status: 201, // 201 Created
         body: req.body
     };
 };
