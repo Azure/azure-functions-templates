dc917b33fa35e780c6e768e932fd63727885d069
Madhuri Koripalli <makoripa@microsoft.com>
2022-05-12 16:30:09 -0700

    cleanup

--- Functions.Templates/Templates/SqlInputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlInputBinding-JavaScript/index.js
@@ -1,3 +1,14 @@
+/**
+ * Sample SQL Input Binding
+ * See https://aka.ms/sqlbindingsinput for more information about using this binding
+ *
+ * These values in function.json should be updated prior to running :
+ *      commandText - This should be the query to execute to retrieve the data values to return
+ *      connectionStringSetting - This should match the name of the app setting containing the connection string
+ *          to use for the SQL connection (default is SqlConnectionString)
+ * @param context The Azure Function runtime context
+ * @param results The array of objects returned by the SQL input binding
+ */
 module.exports = async function (context, results) {
     context.log('Query results: ', JSON.stringify(results));
     return {
--- Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
@@ -1,4 +1,14 @@
-// Visit https://aka.ms/sqlbindingsoutput to learn how to use this output binding
+/**
+ * Sample SQL Output Binding
+ * See https://aka.ms/sqlbindingsoutput for more information about using this binding
+ *
+ * These values in function.json should be updated prior to running :
+ *      commandText - This should be the name of the table that you wish to upsert values to
+ *      connectionStringSetting - This should match the name of the app setting containing the connection string
+ *          to use for the SQL connection (default is SqlConnectionString)
+ * @param context The Azure Function runtime context
+ * @param req The HttpRequest that triggered this function
+ */
 module.exports = async function (context, req) {
     context.log('HTTP trigger with SQL output binding function processed a request.');
 
