2e0bb3ca5558590b7dea245b1049cbbfb2853e60
chgagnon <chgagnon@microsoft.com>
2022-05-12 16:52:19 -0700

    fix

--- Functions.Templates/Templates/SqlInputBinding-Typescript/index.ts
+++ Functions.Templates/Templates/SqlInputBinding-Typescript/index.ts
@@ -5,7 +5,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
  * See https://aka.ms/sqlbindingsinput for more information about using this binding
  *
  * These tasks should be completed prior to running :
- *      1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
+ *      1. Update "commandText" in function.json - this should be the query to execute to retrieve the values being returned
  *      2. Add an app setting named "SqlConnectionString" containing the connection string
  *          to use for the SQL connection
  *      3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
