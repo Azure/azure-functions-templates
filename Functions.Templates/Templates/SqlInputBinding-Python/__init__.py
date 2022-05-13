fe920ec28d6ad28a69b3085100f4d2b1849dd406
Lucy Zhang <luczhan@microsoft.com>
2022-05-13 15:48:35 -0700

    Add sql input output binding templates - python (#1203)

---
+++ Functions.Templates/Templates/SqlInputBinding-Python/__init__.py
@@ -0,0 +1,26 @@
+import azure.functions as func
+import json
+
+def main(req: func.HttpRequest, items: func.SqlRowList) -> func.HttpResponse:
+    """Sample SQL Input Binding
+
+    See https://aka.ms/sqlbindingsinput for more information about using this binding
+
+    These tasks should be completed prior to running :
+    1. Update "commandText" in function.json - this should be the query to execute to retrieve the values being returned
+    2. Add an app setting named "SqlConnectionString" containing the connection string to use for the SQL connection
+    3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
+    4. Update requirements.txt and change the "azure-functions" line to "azure-functions==1.11.3b1" *IMPORTANT* Support for durable functions is not available in this release. A future release will combine SQL bindings and durable functions capabilities.
+
+    Arguments:
+    req: The HttpRequest that triggered this function
+    items: The list of objects returned by the SQL input binding
+    """
+
+    rows = list(map(lambda r: json.loads(r.to_json()), items))
+
+    return func.HttpResponse(
+        json.dumps(rows),
+        status_code=200,
+        mimetype="application/json"
+    )
---
+++ Functions.Templates/Templates/SqlOutputBinding-Python/__init__.py
@@ -0,0 +1,30 @@
+import json
+import azure.functions as func
+
+def main(req: func.HttpRequest, items: func.Out[func.SqlRowList]) -> func.HttpResponse:
+    """Sample SQL Output Binding
+
+    See https://aka.ms/sqlbindingsoutput for more information about using this binding
+
+    These tasks should be completed prior to running :
+    1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
+    2. Add an app setting named "SqlConnectionString" containing the connection string to use for the SQL connection
+    3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
+    4. Update requirements.txt and change the "azure-functions" line to "azure-functions==1.11.3b1" *IMPORTANT* Support for durable functions is not available in this release. A future release will combine SQL bindings and durable functions capabilities.
+
+    Arguments:
+    req: The HttpRequest that triggered this function
+    items: The objects to be upserted to the database
+    """
+
+    # Note that this expects the body to be an array of JSON objects which
+    # have a property matching each of the columns in the table to upsert to.
+    body = json.loads(req.get_body())
+    rows = func.SqlRowList(map(lambda r: func.SqlRow.from_dict(r), body))
+    items.set(rows)
+
+    return func.HttpResponse(
+        body=req.get_body(),
+        status_code=201, # 201 Created
+        mimetype="application/json"
+    )
