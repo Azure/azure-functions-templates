598f70dd77d5611296b742dadfcdb4e48dca6b97
Madhuri Koripalli <makoripa@microsoft.com>
2022-05-13 00:02:09 -0700

    update comments.

--- Functions.Templates/Templates/SqlInputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlInputBinding-JavaScript/index.js
@@ -2,14 +2,16 @@
  * Sample SQL Input Binding
  * See https://aka.ms/sqlbindingsinput for more information about using this binding
  *
- * These values in function.json should be updated prior to running :
- *      commandText - This should be the query to execute to retrieve the data values to return
- *      connectionStringSetting - This should match the name of the app setting containing the connection string
- *          to use for the SQL connection (default is SqlConnectionString)
+ * These tasks should be completed prior to running :
+ *      1. Update "commandText" in function.json - this should be the query to execute to retrieve the values being returned
+ *      2. Add an app setting named "SqlConnectionString" containing the connection string
+ *          to use for the SQL connection
+ *      3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
  * @param context The Azure Function runtime context
- * @param results The array of objects returned by the SQL input binding
+ * @param req The HttpRequest that triggered this function
+ * @param items The array of objects returned by the SQL input binding
  */
-module.exports = async function (context, results) {
+module.exports = async function (context, req, results) {
     context.log('Query results: ', JSON.stringify(results));
     return {
         status: 200,
--- Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
+++ Functions.Templates/Templates/SqlOutputBinding-JavaScript/index.js
@@ -2,17 +2,20 @@
  * Sample SQL Output Binding
  * See https://aka.ms/sqlbindingsoutput for more information about using this binding
  *
- * These values in function.json should be updated prior to running :
- *      commandText - This should be the name of the table that you wish to upsert values to
- *      connectionStringSetting - This should match the name of the app setting containing the connection string
- *          to use for the SQL connection (default is SqlConnectionString)
+ * These tasks should be completed prior to running :
+ *      1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
+ *      2. Add an app setting named "SqlConnectionString" containing the connection string
+ *          to use for the SQL connection
+ *      3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
  * @param context The Azure Function runtime context
  * @param req The HttpRequest that triggered this function
  */
 module.exports = async function (context, req) {
     context.log('HTTP trigger with SQL output binding function processed a request.');
 
-    // Set array to context.bindings for insertion
+    // Set results array to context.bindings for insertion
+    // Note that this expects the body to be a JSON object or array of objects which have a property
+    // matching each of the columns in the table to upsert to.
     context.bindings.results = JSON.stringify(req.body);
 
     context.res = {
