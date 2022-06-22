6a409e1cd7ec485879c142ab756cbc57208ba916
Lilian Kasem <likasem@microsoft.com>
2022-06-22 14:51:43 -0700

    Refactor isolated templates & add dotnet7

---
+++ Functions.Templates/ProjectTemplate_v4.x/CSharp-Isolated/Dotnet7.0/.template.config/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.Azure.Functions.Worker;
+global using Microsoft.Azure.Functions.Worker.Http;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using System.Net;
