46fca46f8b852ec7c6572b0a1180ff250a4aa2a2
Lilian Kasem <likasem@microsoft.com>
2022-06-24 16:40:50 -0700

    fix program.cs

--- Functions.Templates/ProjectTemplate_v4.x/CSharp-Isolated/NetFx48/Program.cs
+++ Functions.Templates/ProjectTemplate_v4.x/CSharp-Isolated/NetFx48/Program.cs
@@ -1,12 +1,19 @@
-using System;
-using System.Diagnostics;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker;
 
-FunctionsDebugger.Enable();
+namespace Company.Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionsDebugger.Enable();
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .Build();
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
 
-host.Run();
+            host.Run();
+        }
+    }
+}
