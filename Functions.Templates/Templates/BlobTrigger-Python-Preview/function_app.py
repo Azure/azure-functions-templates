60ecb4000dbd473ee27a8d3940f3ce5c2da3a524
Shreya Batra <shbatr@microsoft.com>
2022-07-06 15:48:02 -0500

    Added templates

---
+++ Functions.Templates/Templates/BlobTrigger-Python-Preview/function_app.py
@@ -0,0 +1,13 @@
+import logging
+
+import azure.functions as func
+
+app = func.FunctionApp()
+
+@app.function_name(name="BlobTrigger1")
+@app.blob_trigger(arg_name="myblob", path="samples-workitems/{name}",
+                  connection="AzureWebJobsStorage")
+def test_function(myblob: func.InputStream):
+   logging.info(f"Python blob trigger function processed blob \n"
+                f"Name: {myblob.name}\n"
+                f"Blob Size: {myblob.length} bytes")
