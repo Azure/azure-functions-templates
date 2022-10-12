8572c90bd0915d481631893ec235a28ae5b6f8ad
Vasu Bhog <vabhog@microsoft.com>
2022-10-11 17:20:36 -0700

    fix powershell output binding

--- Functions.Templates/Templates/SqlInputBinding-PowerShell/run.ps1
+++ Functions.Templates/Templates/SqlInputBinding-PowerShell/run.ps1
@@ -1,15 +1,13 @@
 <#
 Sample SQL Input Binding
 See https://aka.ms/sqlbindingsinput for more information about using this binding
-These tasks should be completed prior to running :
+These tasks should be completed prior to running:
     1. Update "commandText" in function.json - this should be the query to execute to retrieve the values being returned
-    2. Add an app setting named "SqlConnectionString" containing the connection string
-        to use for the SQL connection
-    3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
+    2. Add an app setting named "SqlConnectionString" containing the connection string to use for the SQL connection
+    3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[4.*, 5.0.0)"
 @param Request The HttpRequest that triggered this function
 @param items The array of objects returned by the SQL input binding
 #>
-
 using namespace System.Net
 
 # Input bindings are passed in via param block.
--- Functions.Templates/Templates/SqlOutputBinding-PowerShell/run.ps1
+++ Functions.Templates/Templates/SqlOutputBinding-PowerShell/run.ps1
@@ -4,7 +4,7 @@ See https://aka.ms/sqlbindingsinput for more information about using this bindin
 These tasks should be completed prior to running :
     1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
     2. Add an app setting named "SqlConnectionString" containing the connection string to use for the SQL connection
-    3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
+    3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[4.*, 5.0.0)"
 @param Request The HttpRequest that triggered this function
 #>
 using namespace System.Net
@@ -15,14 +15,8 @@ param($Request)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell SQL Binding function processed a request."
 
-# Set results array to req_body for upsertion
-# Note that this expects the body to be a JSON object or array of objects which has a property 
-# matching each of the columns in the table to upsert to.
-$req_body = $Request.Body | ConvertTo-Json
+# Update req_body with the body of the request
+$req_body = @{'Insert Values Here'="Insert Value Here"} | ConvertTo-Json
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $req_body
-    ContentType = "application/json"
-})
+Push-OutputBinding -Name results -Value $req_body
