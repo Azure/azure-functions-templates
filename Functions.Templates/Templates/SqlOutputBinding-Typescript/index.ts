36345e2c5245d70bcf484774141424c642da3480
Vasu Bhog <vabhog@microsoft.com>
2022-10-14 11:34:14 -0700

    update instructions to change bundle preview to 4.x (#1269)

--- Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
+++ Functions.Templates/Templates/SqlOutputBinding-Typescript/index.ts
@@ -8,7 +8,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
  *      1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
  *      2. Add an app setting named "SqlConnectionString" containing the connection string
  *          to use for the SQL connection
- *      3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[3.*, 4.0.0)"
+ *      3. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[4.*, 5.0.0)"
  * @param context The Azure Function runtime context
  * @param req The HttpRequest that triggered this function
  */
