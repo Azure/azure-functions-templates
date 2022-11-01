76dec703ef4307d0f620d7d1d5dad001220bc757
ShaneMicro <89389217+ShaneMicro@users.noreply.github.com>
2022-11-01 15:31:14 +0000

    Updated sample templates

--- Functions.Templates/Templates/AuthenticationEventsTrigger-Python/__init__.py
+++ Functions.Templates/Templates/AuthenticationEventsTrigger-Python/__init__.py
@@ -1,1 +1,21 @@
-import OnTokenIssuanceStart
+#Import the event and related version modules.
+from azure.functions.authentication_events.token_issuance_start import *
+from azure.functions.authentication_events.token_issuance_start.actions import ProvideClaimsForToken, Claim
+
+def main(onTokenIssuanceStartRequest: TokenIssuanceStartRequest) -> TokenIssuanceStartResponse:
+    try:
+        # Is the request successful and did the token validation pass.
+        if onTokenIssuanceStartRequest.requestStatus == RequestStatus.Successful:
+            #Fetch information about user from external data store
+
+            # Add new claims to the token's response
+            onTokenIssuanceStartRequest.response.actions.append(ProvideClaimsForToken(
+                [
+                    Claim("DateOfBirth", "01/01/2000"),
+                    Claim("CustomRoles", ["Writer", "Editor"])
+                ]
+            ))
+
+        return onTokenIssuanceStartRequest.response
+    except Exception as e:
+        return FailedRequest.handle(e)
