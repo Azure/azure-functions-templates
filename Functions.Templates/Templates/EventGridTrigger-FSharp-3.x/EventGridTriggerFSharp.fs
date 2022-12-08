9047deda4f72d29fb32feb7524c8cacacd360a89
hossam-nasr <hossam.nasr99@gmail.com>
2022-12-08 12:46:12 -0800

    update eventgrid

---
+++ Functions.Templates/Templates/EventGridTrigger-FSharp-3.x/EventGridTriggerFSharp.fs
@@ -0,0 +1,14 @@
+namespace Company.Function
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+open Microsoft.Azure.EventGrid.Models
+open Microsoft.Azure.WebJobs.Extensions.EventGrid
+open Microsoft.Extensions.Logging
+
+module EventGridTriggerFSharp =
+    [<Functionname>]
+    let run ([<EventGridTrigger>] eventGridEvent: EventGridEvent, log: ILogger) =
+        log.LogInformation(eventGridEvent.Data.ToString())
