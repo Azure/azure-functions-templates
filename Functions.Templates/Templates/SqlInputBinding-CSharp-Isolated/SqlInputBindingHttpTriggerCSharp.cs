c3df6101f63282979ba8f7312710e640f78de0ae
Maddy Koripalli <makoripa@microsoft.com>
2022-12-13 10:32:38 -0800

    add templates for sql in/output isolated bindings

---
+++ Functions.Templates/Templates/SqlInputBinding-CSharp-Isolated/SqlInputBindingHttpTriggerCSharp.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Extension.Sql;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class SqlInputBindingHttpTriggerCSharp
+    {
+        private readonly ILogger _logger;
+
+        public SqlInputBindingHttpTriggerCSharp(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SqlInputBindingHttpTriggerCSharp>();
+        }
+
+        [Function("SqlInputBindingCSharp")]
+        public static IEnumerable<Object> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [SqlInput("SELECT * FROM object",
+            CommandType = System.Data.CommandType.Text,
+            ConnectionStringSetting = "SqlConnectionString")] IEnumerable<Object> result)
+        {
+            _logger.LogInformation("C# HTTP trigger with SQL Input Binding function processed a request.");
+
+            return result;
+        }
+    }
+}
---
+++ Functions.Templates/Templates/SqlOutputBinding-CSharp-Isolated/SqlOutputBindingHttpTriggerCSharp.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Extension.Sql;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class SqlOutputBindingHttpTriggerCSharp
+    {
+        private readonly ILogger _logger;
+
+        public SqlOutputBindingHttpTriggerCSharp(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SqlOutputBindingHttpTriggerCSharp>();
+        }
+
+        // Visit https://aka.ms/sqlbindingsoutput to learn how to use this output binding
+        [Function("SqlOutputBindingCSharp")]
+        [SqlOutput("table", ConnectionStringSetting = "SqlConnectionString")]
+        public static async Task<ToDoItem> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger with SQL Output Binding function processed a request.");
+
+            ToDoItem todoitem = await req.ReadFromJsonAsync<ToDoItem>() ?? new ToDoItem
+                {
+                    Id = "1",
+                    Priority = 1,
+                    Description = "Hello World"
+                };
+            return todoitem;
+        }
+    }
+
+    public class ToDoItem
+    {
+        public string Id { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
+    }
+}
