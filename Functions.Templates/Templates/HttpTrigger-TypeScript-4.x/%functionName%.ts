9d789de460df7834b4dcec77b6414fe2fa7b18e6
Eric Jizba <erijiz@microsoft.com>
2022-12-16 13:33:52 -0800

    Update to HttpResponseInit

--- Functions.Templates/Templates/HttpTrigger-TypeScript-4.x/%functionName%.ts
+++ Functions.Templates/Templates/HttpTrigger-TypeScript-4.x/%functionName%.ts
@@ -1,6 +1,6 @@
-import { app, HttpRequest, HttpResponse, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
-export async function %functionName%(request: HttpRequest, context: InvocationContext): Promise<HttpResponse> {
+export async function %functionName%(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
