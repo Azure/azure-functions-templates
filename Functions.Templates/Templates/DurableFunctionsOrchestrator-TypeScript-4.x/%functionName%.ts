412df747204608d0d146175b0cd656bf67e37ad8
Eric Jizba <erijiz@microsoft.com>
2023-01-31 16:31:45 -0800

    durable 3.0.0-alpha.2 changes

--- Functions.Templates/Templates/DurableFunctionsOrchestrator-TypeScript-4.x/%functionName%.ts
+++ Functions.Templates/Templates/DurableFunctionsOrchestrator-TypeScript-4.x/%functionName%.ts
@@ -1,10 +1,10 @@
-import { app, HttpHandler, HttpRequest, InvocationContext } from '@azure/functions';
+import { app, HttpHandler, HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
 import * as df from 'durable-functions';
-import { ActivityHandler, OrchestrationHandler } from 'durable-functions';
+import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
 
 const activityName = '%functionName%';
 
-const %functionName%Orchestrator: OrchestrationHandler = function* (context) {
+const %functionName%Orchestrator: OrchestrationHandler = function* (context: OrchestrationContext) {
     const outputs = [];
     outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
     outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
@@ -14,23 +14,23 @@ const %functionName%Orchestrator: OrchestrationHandler = function* (context) {
 };
 df.app.orchestration('%functionName%Orchestrator', %functionName%Orchestrator);
 
-const %functionName%: ActivityHandler = (input: string) => {
+const %functionName%: ActivityHandler = (input: string): string => {
     return `Hello, ${input}`;
 };
 df.app.activity(activityName, { handler: %functionName% });
 
-const clientInput = df.input.durableClient();
+const %functionName%HttpStart: HttpHandler = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponse> => {
+    const client = df.getClient(context);
+    const body: unknown = await request.text();
+    const instanceId: string = await client.startNew(request.params.orchestratorName, { input: body });
 
-const %functionName%HttpStart: HttpHandler = async (request: HttpRequest, context: InvocationContext) => {
-    const client = df.getClient(context, clientInput);
-    const body = await request.text();
-    const instanceId = await client.startNew(request.params.orchestratorName, undefined, body);
     context.log(`Started orchestration with ID = '${instanceId}'.`);
+
     return client.createCheckStatusResponse(request, instanceId);
 };
 
 app.http('%functionName%HttpStart', {
     route: 'orchestrators/{orchestratorName}',
-    extraInputs: [clientInput],
+    extraInputs: [df.input.durableClient()],
     handler: %functionName%HttpStart,
 });
