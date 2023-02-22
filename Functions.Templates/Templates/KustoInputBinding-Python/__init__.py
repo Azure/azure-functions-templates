27cb8eb35585e472487420854bcd693e6e79ab5f
Ramachandran G <ramacg@microsoft.com>
2023-02-22 18:29:19 +0530

    * Minor fixes in templates

--- Functions.Templates/Templates/KustoInputBinding-Python/__init__.py
+++ Functions.Templates/Templates/KustoInputBinding-Python/__init__.py
@@ -8,7 +8,8 @@ def main(req: func.HttpRequest, products: str) -> func.HttpResponse:
     These tasks should be completed prior to running :
        1. Update "kqlCommand" in function.json - this should be a KQLQuery or Function being used for the query
        2. Update "database" in function.json - this should be the database the query has to be run against
-       3. Optionally update "kqlParameters" in function.json - this should be the runtime predicates that can be used for the kqlCommand
+       3. Optionally update "kqlParameters" in function.json - this should be the runtime predicates that can be used for the kqlCommand.
+       Note that in the sample a sample context path parameter is provided as a sample for parameter template. Please do change this as well
        4. Add an app setting named "KustoConnectionString" containing the connection string
            to use for the Kusto connection
        5. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[4.*, 5.0.0)"
