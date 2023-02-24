ac97c60ea01c5b4c71e318fbeef53a225c992d4f
Maddy Koripalli <makoripa@microsoft.com>
2023-02-24 10:23:58 -0800

    fix references

--- Functions.Templates/Templates/SqlOutputBinding-CSharp/SqlOutputBindingCSharp.cs
+++ Functions.Templates/Templates/SqlOutputBinding-CSharp/SqlOutputBindingCSharp.cs
@@ -1,10 +1,11 @@
-using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Company.Function
 {
