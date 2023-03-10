6e1566a2fbf41045a3eae958a6ade70241f1e54c
Ramachandran G <ramacg@microsoft.com>
2023-03-10 08:32:02 +0530

    Minor fixes in imports

--- Functions.Templates/Templates/KustoOutputBinding-CSharp/KustoOutputBindingCSharp.cs
+++ Functions.Templates/Templates/KustoOutputBinding-CSharp/KustoOutputBindingCSharp.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Kusto;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Company.Function 
 {
