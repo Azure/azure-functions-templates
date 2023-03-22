17fc2e8c939b7fb94cbace6b473c8ff807b8187e
Gavin Aguiar <gavin@GavinPC>
2023-03-22 17:40:31 -0500

    Updated templates based on the spec

--- Functions.Templates/Templates-v2/EventHubTrigger-Python/function_body.py
+++ Functions.Templates/Templates-v2/EventHubTrigger-Python/function_body.py
@@ -1,6 +1,6 @@
 
-@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="$(EVENTHUB_NAME)",
-                               connection="$(EVENTHUB_CONNECTION_STRING)") 
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="$(EVENTHUB_NAME_INPUT)",
+                               connection="$(CONNECTION_STRING_INPUT)") 
 def $(FUNCTION_NAME_INPUT)(azeventhub: EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s',
                 azeventhub.get_body().decode('utf-8'))
