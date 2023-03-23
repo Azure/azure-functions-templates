8b28274885b835c34f58c7a013e3a4d8e313813c
Gavin Aguiar <gavin@GavinPC>
2023-03-23 17:22:23 -0500

    Removed importing and registering blueprints

--- Functions.Templates/Templates-v2/EventHubTrigger-Python/blueprint.py
+++ Functions.Templates/Templates-v2/EventHubTrigger-Python/blueprint.py
@@ -1,3 +1,10 @@
+# Register this blueprint by adding the following line of code 
+# to your entry point file.  
+# app.register_functions($(BLUEPRINT_FILENAME)) 
+# 
+#Please refer to https://aka.ms/azure-functions-python-blueprints
+
+
 from azure.functions import Blueprint
 
 $(BLUEPRINT_FILENAME) = Blueprint()
--- Functions.Templates/Templates-v2/EventHubTrigger-Python/blueprint_register.py
+++
@@ -1,1 +0,0 @@
-@app.register($(BLUEPRINT_FILENAME))
