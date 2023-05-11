82d86de0510fa7e7fef812bfd8731d3756d65ac0
pthummar <pthummar@microsoft.com>
2023-05-11 12:45:50 -0500

    Added eventgrid trigger template for python

---
+++ Functions.Templates/Templates-v2/EventGridTrigger-Python/blueprint_body.py
@@ -0,0 +1,16 @@
+
+@$(BLUEPRINT_FILENAME).event_grid_trigger(arg_name="azeventgrid")
+def $(FUNCTION_NAME_INPUT)(azeventgrid: EventGridEvent):
+
+    result = json.dumps({
+        'id': azeventgrid.id,
+        'data': azeventgrid.get_json(),
+        'topic': azeventgrid.topic,
+        'subject': azeventgrid.subject,
+        'event_type': azeventgrid.event_type,
+    })
+
+    logging.info('Python EventGrid trigger processed an event: %s', result)
+
+
+
---
+++ Functions.Templates/Templates-v2/EventGridTrigger-Python/function_app.py
@@ -0,0 +1,17 @@
+from azure.functions import FunctionApp, EventGridEvent
+import logging
+
+app = FunctionApp()
+
+@app.event_grid_trigger(arg_name="azeventgrid")
+def $(FUNCTION_NAME_INPUT)(azeventgrid: EventGridEvent):
+
+    result = json.dumps({
+        'id': azeventgrid.id,
+        'data': azeventgrid.get_json(),
+        'topic': azeventgrid.topic,
+        'subject': azeventgrid.subject,
+        'event_type': azeventgrid.event_type,
+    })
+
+    logging.info('Python EventGrid trigger processed an event: %s', result)
