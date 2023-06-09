338bbfd90ef8e41460d97ce8d01071df3f3ec442
Gavin Aguiar <gavin@GavinPC>
2023-06-09 14:42:10 -0500

    Added properties to resources.resx

--- Functions.Templates/Templates-v2/EventGridTrigger-Python/blueprint_body.py
+++ Functions.Templates/Templates-v2/EventGridTrigger-Python/blueprint_body.py
@@ -1,6 +1,3 @@
 @$(BLUEPRINT_FILENAME).event_grid_trigger(arg_name="azeventgrid")
 def $(FUNCTION_NAME_INPUT)(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
-
-
-
--- Functions.Templates/Templates-v2/EventGridTrigger-Python/function_app.py
+++ Functions.Templates/Templates-v2/EventGridTrigger-Python/function_app.py
@@ -7,4 +7,3 @@ app = func.FunctionApp()
 @app.event_grid_trigger(arg_name="azeventgrid")
 def $(FUNCTION_NAME_INPUT)(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
-
