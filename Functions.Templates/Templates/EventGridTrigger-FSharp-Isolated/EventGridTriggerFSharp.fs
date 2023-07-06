9387df7535e6b9e3efb3b991f5dbd633c7f67236
Lilian Kasem <likasem@microsoft.com>
2023-07-05 17:44:44 -0700

    Update fsharp template

--- Functions.Templates/Templates/EventGridTrigger-FSharp-Isolated/EventGridTriggerFSharp.fs
+++ Functions.Templates/Templates/EventGridTrigger-FSharp-Isolated/EventGridTriggerFSharp.fs
@@ -1,23 +1,17 @@
 namespace Company.Function
+
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
 open System
+open Azure.Messaging;
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Extensions.Logging
 
 module EventGridTriggerFSharp =
-    type MyEvent =
-        { Id: string
-          Topic: string
-          Subject: string
-          EventType: string
-          EventTime: DateTime
-          Data: object }
-
     [<Function("EventGridTriggerFSharp")>]
-    let run ([<EventGridTrigger>] eventGridEvent: MyEvent, context: FunctionContext) =
+    let run ([<EventGridTrigger>] cloudEvent: CloudEvent, context: FunctionContext) =
         let logger =
             context.GetLogger("EventGridTriggerFSharp")
 
-        log.LogInformation(eventGridEvent.Data.ToString())
+        log.LogInformation(cloudEvent.Subject)
