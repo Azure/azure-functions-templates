f0d6a39f3f3bd54eb3852c75aeae1f721e31ca46
Lilian Kasem <likasem@microsoft.com>
2023-07-25 12:02:21 -0700

    Update dotnet-isolated queue storage templates (#1409)

--- Functions.Templates/Templates/QueueTrigger-FSharp-Isolated/QueueTriggerFSharp.fs
+++ Functions.Templates/Templates/QueueTrigger-FSharp-Isolated/QueueTriggerFSharp.fs
@@ -1,6 +1,7 @@
 namespace Company.Function
 
 open System
+open Azure.Storage.Queues.Models;
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Extensions.Logging
 
@@ -8,11 +9,11 @@ module QueueTriggerFSharp =
     [<Function("QueueTriggerFSharp")>]
     let run
         (
-            [<QueueTrigger("PathValue", Connection = "ConnectionValue")>] myQueueItem: string,
+            [<QueueTrigger("PathValue", Connection = "ConnectionValue")>] message: QueueMessage,
             context: FunctionContext
         ) =
         let msg =
-            sprintf "F# Queue trigger function processed: %s" myQueueItem
+            sprintf "F# Queue trigger function processed: %s" message.MessageText
 
         let logger = context.GetLogger "QueueTriggerFSharp"
         log.LogInformation msg
