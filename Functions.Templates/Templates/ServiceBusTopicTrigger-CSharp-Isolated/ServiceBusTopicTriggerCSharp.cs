f0140ff72b60169189beb0ab9761fb429e4df837
Lilian Kasem <likasem@microsoft.com>
2023-08-01 14:30:24 -0700

    Update dotnet-isolated servicebus templates (#1410)

--- Functions.Templates/Templates/ServiceBusTopicTrigger-CSharp-Isolated/ServiceBusTopicTriggerCSharp.cs
+++ Functions.Templates/Templates/ServiceBusTopicTrigger-CSharp-Isolated/ServiceBusTopicTriggerCSharp.cs
@@ -1,4 +1,5 @@
 using System;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -6,17 +7,19 @@ namespace Company.Function
 {
     public class ServiceBusTopicTriggerCSharp
     {
-        private readonly ILogger _logger;
+        private readonly ILogger<ServiceBusTopicTriggerCSharp> _logger;
 
-        public ServiceBusTopicTriggerCSharp(ILoggerFactory loggerFactory)
+        public ServiceBusTopicTriggerCSharp(ILogger<ServiceBusTopicTriggerCSharp> logger)
         {
-            _logger = loggerFactory.CreateLogger<ServiceBusTopicTriggerCSharp>();
+            _logger = logger;
         }
 
-        [Function("ServiceBusTopicTriggerCSharp")]
-        public void Run([ServiceBusTrigger("TopicNameValue", "SubscriptionNameValue", Connection = "ConnectionValue")] string mySbMsg)
+        [Function(nameof(ServiceBusTopicTriggerCSharp))]
+        public void Run([ServiceBusTrigger("TopicNameValue", "SubscriptionNameValue", Connection = "ConnectionValue")] ServiceBusReceivedMessage message)
         {
-            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
         }
     }
 }
