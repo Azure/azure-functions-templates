8c4498721e53318a2baaa9d890ea4df3159dc2ec
Surgupta <surgupta@microsoft.com>
2023-08-25 11:06:20 -0700

    Update timer trigger template for net isolated

--- Functions.Templates/Templates/TimerTrigger-CSharp-Isolated/TimerTriggerCSharp.cs
+++ Functions.Templates/Templates/TimerTrigger-CSharp-Isolated/TimerTriggerCSharp.cs
@@ -14,26 +14,10 @@ namespace Company.Function
         }
 
         [Function("TimerTriggerCSharp")]
-        public void Run([TimerTrigger("ScheduleValue")] MyInfo myTimer)
+        public void Run([TimerTrigger("ScheduleValue")]TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
         }
-    }
-
-    public class MyInfo
-    {
-        public MyScheduleStatus ScheduleStatus { get; set; }
-
-        public bool IsPastDue { get; set; }
-    }
-
-    public class MyScheduleStatus
-    {
-        public DateTime Last { get; set; }
-
-        public DateTime Next { get; set; }
-
-        public DateTime LastUpdated { get; set; }
     }
 }
