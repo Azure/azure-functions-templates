3b5943e1158aea5080711662ae4d355e5af1836b
Lilian Kasem <likasem@microsoft.com>
2023-08-30 17:19:02 -0700

    Add link

--- Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
+++ Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
@@ -8,7 +8,8 @@ let main args =
             .ConfigureFunctionsWorkerDefaults()
             .Build()
 
-        // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below:
+        // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below.
+        // Learn more about this here: https://go.microsoft.com/fwlink/?linkid=2245587
         // ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
         //     appBuilder.ConfigureCosmosDBExtension() |> ignore
         //     appBuilder.ConfigureBlobStorageExtension() |> ignore
