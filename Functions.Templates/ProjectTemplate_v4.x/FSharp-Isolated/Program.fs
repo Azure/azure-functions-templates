36f82a60c2c80d73a6fa9efa4bd01bb669d9ea39
Lilian Kasem <likasem@microsoft.com>
2023-08-30 17:08:17 -0700

    Update fsharp packages

--- Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
+++ Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
@@ -1,4 +1,5 @@
 open Microsoft.Extensions.Hosting
+open Microsoft.Azure.Functions.Worker
 
 [<EntryPoint>]
 let main args =
@@ -6,6 +7,13 @@ let main args =
         HostBuilder()
             .ConfigureFunctionsWorkerDefaults()
             .Build()
+
+        // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below:
+        // ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
+        //     appBuilder.ConfigureCosmosDBExtension() |> ignore
+        //     appBuilder.ConfigureBlobStorageExtension() |> ignore
+        //     appBuilder.ConfigureTablesExtension() |> ignore
+        // ) |> ignore
 
     host.Run()
     0
