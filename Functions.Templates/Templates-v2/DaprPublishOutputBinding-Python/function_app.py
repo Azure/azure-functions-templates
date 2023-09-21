31e21c4c402f1b075bd061552f007fb53056e079
MD Ashique <noorani.ashique5@gmail.com>
2023-09-21 18:03:28 +0530

    Add Dapr Python V2 template
    Signed-off-by: MD Ashique <noorani.ashique5@gmail.com>

--- Functions.Templates/Templates-v2/DaprPublishOutputBinding-Python/function_app.py
+++ Functions.Templates/Templates-v2/DaprPublishOutputBinding-Python/function_app.py
@@ -8,12 +8,23 @@ dapp = func.DaprFunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @dapp.schedule(schedule="*/10 * * * * *", arg_name="myTimer", run_on_startup=True)
 @dapp.dapr_publish_output(arg_name="pubEvent", pub_sub_name="pubsub", topic="A")
 def $(FUNCTION_NAME_INPUT)(myTimer, pubEvent: func.Out[bytes]) -> None:
+    """
+    Sample Dapr Publish Output Binding
+    See https://aka.ms/azure-function-dapr-publish-output-binding for more information about using this binding
+    
+    These tasks should be completed prior to running :
+         1. Install Dapr
+         2. Change the bundle name in host.json to "Microsoft.Azure.Functions.ExtensionBundle.Preview" and the version to "[4.*, 5.0.0)"
+    Run the app with below steps
+         1. Start function app with Dapr: dapr run --app-id functionapp --app-port 3001 --dapr-http-port 3501 -- func host start
+         2. Function will be invoked by Timer trigger and publish messages to message bus.
+    """
     logging.info('Python DaprPublish output binding function processed a request.')
     payload = f"Invoked by Timer trigger: Hello, World! The time is {datetime.datetime.now()}"
     pubEvent.set(json.dumps({"payload": payload}).encode('utf-8'))
 
 
-# Dapr topic trigger
+# Below Azure function will receive message published on topic A, and it will log the message
 @dapp.function_name(name="DaprTopicTriggerFuncApp")
 @dapp.dapr_topic_trigger(arg_name="subEvent", pub_sub_name="pubsub", topic="A")
 def main(subEvent) -> None:
