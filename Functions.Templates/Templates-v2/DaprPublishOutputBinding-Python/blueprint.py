a6191df867df8a992304258b31090700e1d375eb
Naren Soni <soninaren@users.noreply.github.com>
2023-10-05 17:33:37 -0700

    Merging from dev to master (#1466)

---
+++ Functions.Templates/Templates-v2/DaprPublishOutputBinding-Python/blueprint.py
@@ -0,0 +1,13 @@
+# Register this blueprint by adding the following line of code 
+# to your entry point file.  
+# app.register_functions($(BLUEPRINT_FILENAME)) 
+# 
+# Please refer to https://aka.ms/azure-functions-python-blueprints
+
+import datetime
+import json
+import azure.functions as func
+import logging
+
+
+$(BLUEPRINT_FILENAME) = func.Blueprint()
