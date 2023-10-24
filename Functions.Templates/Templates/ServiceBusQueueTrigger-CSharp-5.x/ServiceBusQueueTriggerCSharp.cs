f19d2f108102a2210c2410b3642d9d70b44b2d07
soninaren <nasoni@microsoft.com>
2023-10-24 10:18:40 -0700

    Track 2 extension changes

---
+++ Functions.Templates/Templates/ServiceBusQueueTrigger-CSharp-5.x/ServiceBusQueueTriggerCSharp.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class ServiceBusQueueTriggerCSharp
+    {
+        [FunctionName("ServiceBusQueueTriggerCSharp")]
+        public void Run([ServiceBusTrigger("QueueNameValue", Connection = "ConnectionValue")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
