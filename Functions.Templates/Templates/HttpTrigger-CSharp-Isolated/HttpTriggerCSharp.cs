5dd39dfa0036bdf1d70074f22e43fa82692a0669
Sarah Vu <sarahvu@microsoft.com>
2023-10-24 14:44:48 -0700

    initial wip

--- Functions.Templates/Templates/HttpTrigger-CSharp-Isolated/HttpTriggerCSharp.cs
+++ Functions.Templates/Templates/HttpTrigger-CSharp-Isolated/HttpTriggerCSharp.cs
@@ -1,7 +1,12 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+#if NET6_0_OR_GREATER
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+#else
+using Microsoft.Azure.Functions.Worker.Http;
+#endif
 
 namespace Company.Function
 {
@@ -15,6 +20,12 @@ namespace Company.Function
         }
 
         [Function("HttpTriggerCSharp")]
+#if NET6_0_OR_GREATER
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+#else
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
@@ -26,5 +37,6 @@ namespace Company.Function
 
             return response;
         }
+#endif
     }
 }
