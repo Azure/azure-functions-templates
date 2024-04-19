f03f4516a9a997df83e09bb8186549588488d375
Daniel Castro <dacastr@microsoft.com>
2024-04-19 14:51:33 -0700

    Add config files and update name

--- Functions.Templates/Templates/EventGridBlobTrigger-JavaScript/index.js
+++ Functions.Templates/Templates/EventGridBlobTrigger-JavaScript/index.js
@@ -1,3 +1,3 @@
 module.exports = async function (context, myEventGridBlob) {
-    context.log("JavaScript Event Grid blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myEventGridBlob.length, "Bytes");
+    context.log("JavaScript blob storage trigger (using Event Grid) function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myEventGridBlob.length, "Bytes");
 };
