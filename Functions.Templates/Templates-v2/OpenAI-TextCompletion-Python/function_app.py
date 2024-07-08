e7dd06b9327b8c20e733919f31934973dd1df3e1
Gavin Aguiar <gaaguiar@microsoft.com>
2024-07-08 11:51:52 -0500

    Open AI python templates

--- Functions.Templates/Templates-v2/OpenAI-TextCompletion-Python/function_app.py
+++ Functions.Templates/Templates-v2/OpenAI-TextCompletion-Python/function_app.py
@@ -2,11 +2,11 @@ import logging
 import azure.functions as func
 
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.$(AUTHLEVEL_INPUT))
+app = func.FunctionApp()
                        
 
-@app.route(route="whois/{name}", methods=["GET"])
-@app.text_completion_input(arg_name="response", prompt="Who is {name}?", max_tokens="100", model = "%CHAT_MODEL_DEPLOYMENT_NAME%")
+@app.route(route="$(FUNCTION_NAME_INPUT)", methods=["GET"], auth_level=func.AuthLevel.$(AUTHLEVEL_INPUT))
+@app.text_completion_input(arg_name="response", prompt="{Prompt}", max_tokens="100", model="$(CHAT_MODEL_NAME)")
 def $(FUNCTION_NAME_INPUT)(req: func.HttpRequest, response: str) -> func.HttpResponse:
     response_json = json.loads(response)
     return func.HttpResponse(response_json["content"], status_code=200)
