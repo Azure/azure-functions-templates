e887de7e3252440f42273514cbed050062771de2
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2024-07-10 08:11:53 +0530

    Add MySqlInputBinding and MySqlOutputBinding templates
    Add MySqlInputBinding and MySqlOutputBinding templates for CSharp, CSharpIsolated, JavaScript, PowerShell, Python, Typescript

---
+++ Functions.Templates/Templates/MySqlInputBinding-JavaScript/readme.md
@@ -0,0 +1,7 @@
+# MySql Input Binding - JavaScript
+
+The `MySql Input Binding` makes it easy to retrieve data from a database, returning the output of the query or stored procedure to the function.
+
+## How it works
+
+For a `MySql Input Binding` to work, you can provide the query to retrive data from an existing object in the database. For instance, you can set the query to `Select * From [table1]` to query `[table1]` in the `commandText` value in your `function.json`.
---
+++ Functions.Templates/Templates/MySqlOutputBinding-Python/__init__.py
@@ -0,0 +1,34 @@
+import json
+
+from azure.functions import HttpRequest, HttpResponse, Out, MySqlRowList, MySqlRow
+
+def main(req: HttpRequest, items: Out[MySqlRowList]) -> HttpResponse:
+    """Sample MySQL Output Binding
+
+    See https://aka.ms/sqlbindingsoutput for more information about using this binding
+
+    *IMPORTANT*
+        Local Development : You must have version >= 4.0.5030 of the Azure Function Core Tools installed.
+
+    See https://github.com/Azure/azure-functions-sql-extension/issues/250 for the current state of Python support for the MySQL binding
+
+    These tasks should be completed prior to running :
+    1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
+    2. Add an app setting named "MySqlConnectionString" containing the connection string to use for the MySQL connection
+
+    Arguments:
+    req: The HttpRequest that triggered this function
+    items: The objects to be upserted to the database
+    """
+
+    # Note that this expects the body to be an array of JSON objects which
+    # have a property matching each of the columns in the table to upsert to.
+    body = json.loads(req.get_body())
+    rows = MySqlRowList(map(lambda r: MySqlRow.from_dict(r), body))
+    items.set(rows)
+
+    return HttpResponse(
+        body=req.get_body(),
+        status_code=201, # 201 Created
+        mimetype="application/json"
+    )
