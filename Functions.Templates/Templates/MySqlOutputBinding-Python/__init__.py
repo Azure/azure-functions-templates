07b2b13ead735cc56d81745321b92ee0ceac06b0
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2024-07-10 13:05:46 +0530

    modify small mistakes

--- Functions.Templates/Templates/MySqlOutputBinding-Python/__init__.py
+++ Functions.Templates/Templates/MySqlOutputBinding-Python/__init__.py
@@ -5,12 +5,10 @@ from azure.functions import HttpRequest, HttpResponse, Out, MySqlRowList, MySqlR
 def main(req: HttpRequest, items: Out[MySqlRowList]) -> HttpResponse:
     """Sample MySQL Output Binding
 
-    See https://aka.ms/sqlbindingsoutput for more information about using this binding
+    See https://aka.ms/mysqlbindingsoutput for more information about using this binding
 
     *IMPORTANT*
         Local Development : You must have version >= 4.0.5030 of the Azure Function Core Tools installed.
-
-    See https://github.com/Azure/azure-functions-sql-extension/issues/250 for the current state of Python support for the MySQL binding
 
     These tasks should be completed prior to running :
     1. Update "commandText" in function.json - this should be the name of the table that you wish to upsert values to
