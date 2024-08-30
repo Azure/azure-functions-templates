f27baf3eb1aeb284739721d63c9c68059e3f27a6
Jinjun Liang <kumkee@users.noreply.github.com>
2024-08-31 09:22:43 +1200

    rewrite C# lambda function in F# (#1517)
    This is to fix issue https://github.com/Azure/azure-functions-templates/issues/1514

--- Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
+++ Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
@@ -1,25 +1,26 @@
 open Microsoft.Extensions.Hosting
 open Microsoft.Azure.Functions.Worker
-open Microsoft.Extensions.DependencyInjection;
+open Microsoft.Extensions.DependencyInjection
 
 [<EntryPoint>]
 let main args =
     let host =
         HostBuilder()
             .ConfigureFunctionsWorkerDefaults()
-            .ConfigureServices(services => {
-                services.AddApplicationInsightsTelemetryWorkerService();
-                services.ConfigureFunctionsApplicationInsights();
-            })
+            .ConfigureServices(fun services ->
+                services.AddApplicationInsightsTelemetryWorkerService()
+                |> ignore
+
+                services.ConfigureFunctionsApplicationInsights() |> ignore)
             .Build()
 
-        // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below.
-        // Learn more about this here: https://go.microsoft.com/fwlink/?linkid=2245587
-        // ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
-        //     appBuilder.ConfigureCosmosDBExtension() |> ignore
-        //     appBuilder.ConfigureBlobStorageExtension() |> ignore
-        //     appBuilder.ConfigureTablesExtension() |> ignore
-        // ) |> ignore
+    // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below.
+    // Learn more about this here: https://go.microsoft.com/fwlink/?linkid=2245587
+    // ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
+    //     appBuilder.ConfigureCosmosDBExtension() |> ignore
+    //     appBuilder.ConfigureBlobStorageExtension() |> ignore
+    //     appBuilder.ConfigureTablesExtension() |> ignore
+    // ) |> ignore
 
     host.Run()
     0
