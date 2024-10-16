84f7cafedca0759d34b4150ab3d104746b72ae77
Matthew Henderson <mattchenderson@users.noreply.github.com>
2024-10-16 11:01:38 -0700

    Use IHostApplicationBuilder for .NET Worker V2 SDK (#1574)

--- Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
+++ Functions.Templates/ProjectTemplate_v4.x/FSharp-Isolated/Program.fs
@@ -1,12 +1,16 @@
-open Microsoft.Extensions.Hosting
 open Microsoft.Azure.Functions.Worker
+#if (!FrameworkShouldUseV1Dependencies)
+open Microsoft.Azure.Functions.Worker.Builder
+#endif
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 
+#if (FrameworkShouldUseV1Dependencies)
 [<EntryPoint>]
 let main args =
     let host =
         HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureFunctionsWebApplication()
             .ConfigureServices(fun services ->
                 services.AddApplicationInsightsTelemetryWorkerService()
                 |> ignore
@@ -14,9 +18,9 @@ let main args =
                 services.ConfigureFunctionsApplicationInsights() |> ignore)
             .Build()
 
-    // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below.
+    // If using the Cosmos DB, Blob or Tables extension, you need to configure the extensions manually using the extension methods below.
     // Learn more about this here: https://go.microsoft.com/fwlink/?linkid=2245587
-    // ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
+    // ConfigureFunctionsWebApplication(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
     //     appBuilder.ConfigureCosmosDBExtension() |> ignore
     //     appBuilder.ConfigureBlobStorageExtension() |> ignore
     //     appBuilder.ConfigureTablesExtension() |> ignore
@@ -24,3 +28,24 @@ let main args =
 
     host.Run()
     0
+#else
+[<EntryPoint>]
+let main args =
+    let builder = FunctionsApplication.CreateBuilder(args)
+
+    builder.ConfigureFunctionsWebApplication() |> ignore
+
+    builder.Services
+        .AddApplicationInsightsTelemetryWorkerService()
+        .ConfigureFunctionsApplicationInsights() 
+    |> ignore
+            
+    // If using the Cosmos DB, Blob or Tables extension, you need to configure the extensions manually using the extension methods below.
+    // Learn more about this here: https://go.microsoft.com/fwlink/?linkid=2245587
+    // builder.ConfigureCosmosDBExtension() |> ignore
+    // builder.ConfigureBlobStorageExtension() |> ignore
+    // builder.ConfigureTablesExtension() |> ignore
+
+    builder.Build().Run()
+    0
+#endif
