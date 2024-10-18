7bdec6c930117405a4fd116d37e22ba0da5d25ac
Lilian Kasem <liliankasem@gmail.com>
2024-10-18 09:48:29 -0700

    Revert "Add name query template on dotnet isolated.  (#1575)"
    This reverts commit c6ede75673d6b69126e1c50ee6654608036d52ad.

--- Functions.Templates/Templates/HttpTrigger-CSharp-Isolated/HttpTriggerCSharp.cs
+++ Functions.Templates/Templates/HttpTrigger-CSharp-Isolated/HttpTriggerCSharp.cs
@@ -1,9 +1,8 @@
 #if( NetCore )
-using System.Text.Json;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Function
 {
@@ -17,35 +16,16 @@ namespace Company.Function
         }
 
         [Function("HttpTriggerCSharp")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post", Route = null)] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            string? name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic? data = null;
-
-            if (!string.IsNullOrEmpty(requestBody))
-            {
-                data = JsonSerializer.Deserialize<dynamic>(requestBody);
-                name = name ?? data?.name;
-            }
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
 }
 #endif
 #if( NetFramework )
-using System.IO;
-using System.Text.Json;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -62,29 +42,14 @@ namespace Company.Function
         }
 
         [Function("HttpTriggerCSharp")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.AuthLevelValue, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var queryParams = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
-            string name = queryParams["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = null;
-
-            if (!string.IsNullOrEmpty(requestBody))
-            {
-                data = JsonSerializer.Deserialize<dynamic>(requestBody);
-                name = name ?? data?.name;
-            }
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            await response.WriteStringAsync(responseMessage);
+
+            response.WriteString("Welcome to Azure Functions!");
 
             return response;
         }
