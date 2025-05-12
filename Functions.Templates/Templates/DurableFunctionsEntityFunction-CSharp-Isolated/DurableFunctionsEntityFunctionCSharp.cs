1e52ee039ee853017e853e102db3ded78698c34a
Andy Staples <andystaples@microsoft.com>
2025-05-12 16:20:26 -0600

    Fix naming

---
+++ Functions.Templates/Templates/DurableFunctionsEntityFunction-CSharp-Isolated/DurableFunctionsEntityFunctionCSharp.cs
@@ -0,0 +1,41 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function;
+
+public static class DurableFunctionsEntityFunctionCSharp
+{
+    [Function(nameof(Counter))]
+    public static Task DispatchAsync([EntityTrigger] TaskEntityDispatcher dispatcher)
+    {
+        return dispatcher.DispatchAsync(operation =>
+        {
+            if (operation.State.GetState(typeof(int)) is null)
+            {
+                operation.State.SetState(0);
+            }
+
+            switch (operation.Name.ToLowerInvariant())
+            {
+                case "add":
+                    int state = operation.State.GetState<int>();
+                    state += operation.GetInput<int>();
+                    operation.State.SetState(state);
+                    return new(state);
+                case "reset":
+                    operation.State.SetState(0);
+                    break;
+                case "get":
+                    return new(operation.State.GetState<int>());
+                case "delete": 
+                    operation.State.SetState(null);
+                    break; 
+            }
+
+            return default;
+        });
+    }
+}
