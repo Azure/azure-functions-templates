e443114d6294af70d4d63b7964c2d6146a4079b2
Lilian Kasem <likasem@microsoft.com>
2025-09-19 10:56:22 -0700

    Setup mcp tool template for .net

---
+++ Functions.Templates/Templates/McpToolTrigger-CSharp-Isolated/HelloTool.cs
@@ -0,0 +1,25 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Mcp;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function;
+
+public class HelloTool
+{
+    private ILogger<HelloTool> _logger;
+
+    public HelloTool(ILogger<HelloTool> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(HelloTool))]
+    public string Run(
+        [McpToolTrigger(nameof(HelloTool), "Responds to the user with a hello message.")] ToolInvocationContext context,
+        [McpToolProperty(nameof(name), "string", "The name of the person to greet.")] string? name
+    )
+    {
+        _logger.LogInformation("C# MCP tool trigger function processed a request.");
+        return $"Hello, {name ?? "world"}! This is an MCP Tool!";
+    }
+}
